0a7c7a1335575f782e90fc857cd91de03bd3a830
SuinLee21 <suin7473@naver.com>
2025-01-26 14:35:45 +0900

    feat: Add getAllPlace method

--- src/api/place/place.controller.ts
+++ src/api/place/place.controller.ts
@@ -1,7 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
 import { PlaceService } from './place.service';
+import { AllPlaceResponseDto, GetAllPlaceDto } from './place.dto';
 
 @Controller('place')
 export class PlaceController {
     constructor(private placeService: PlaceService) { }
+
+    @Get()
+    async getAllPlace(
+        @Body() getAllPlaceDto: GetAllPlaceDto
+    ): Promise<AllPlaceResponseDto> {
+        return this.placeService.getAllPlace(getAllPlaceDto);
+    }
 }
