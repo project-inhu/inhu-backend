d64fdc1145da3aa6883a25e76c91bdcd5b9d1380
SuinLee21 <suin7473@naver.com>
2025-01-30 13:44:30 +0900

    refactor: Extract business logic from repository in getAllPlace method

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { PrismaModule } from './common/module/prisma/prisma.module';
 import { PlaceModule } from './api/place/place.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { KeywordModule } from './api/keyword/keyword.module';
 
 @Module({
   imports: [
