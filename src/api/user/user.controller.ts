362a350730bc8c3cb7e2d1439269ed74cd47815d
hj-lab <heeju03180@naver.com>
2025-01-30 17:53:28 +0900

    feat: Add getMyInfo method

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from '@prisma/client';
+import { GetMyInfoResponseDto } from './dto/user-info.dto';
 
 @Controller('user')
-export class UserController { }
+export class UserController {
+    constructor(private userService: UserService) { };
+
+    // * 내 정보 보기 (프로필, 닉네임)
+    @Get()
+    // TODO : authguard 추가 / auth 폴더에서 토큰 발급 api 추가
+    async getMyInfo(@Req() req: any): Promise<GetMyInfoResponseDto> {
+        const { idx } = req.user;
+        return this.userService.getMyInfo(idx);
+    }
+}
--- src/api/user/user.service.ts
+++ src/api/user/user.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/common/module/prisma/prisma.service';
+import { GetMyInfoResponseDto } from './dto/user-info.dto';
 
 @Injectable()
-export class UserService { }
+export class UserService {
+    constructor(private prisma: PrismaService) { }
+    // * 내 정보 보기 (마이페이지 - 프로필, 닉네임)
+    // 그러면 로그인할때 닉네임 정해지는거임?
+    async getMyInfo(idx: number): Promise<GetMyInfoResponseDto> {
+        // 토큰에서 내 idx값 가져와서 그 idx에 해당하는 프로필, 닉네임 가져오기
+        const user = await this.prisma.user.findUniqueOrThrow({
+            where: { idx },
+            select: {
+                nickname: true,
+                profileImagePath: true
+            }
+        })
+
+        return user;
+    }
+
+    // * 내 정보 수정하기
+
+    // * 회원 탈퇴
+}
