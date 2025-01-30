92c513510f6c3471ff61b752d5b557cfb8c21bab
SuinLee21 <suin7473@naver.com>
2025-01-30 13:44:30 +0900

    refactor: Extract business logic from repository in getAllPlace method

--- src/api/place/place.module.ts
+++ src/api/place/place.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { PlaceController } from './place.controller';
 import { PlaceService } from './place.service';
 import { PlaceRepository } from './place.repository';
+import { KeywordModule } from '../keyword/keyword.module';
 
 @Module({
+  imports: [KeywordModule],
   controllers: [PlaceController],
   providers: [PlaceService, PlaceRepository]
 })
