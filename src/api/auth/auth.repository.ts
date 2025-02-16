36af326d95bf3e5e6af3a27808632ded218a4609
SuinLee21 <suin7473@naver.com>
2025-02-17 03:28:07 +0900

    refactor: Add comments and remove unused functions

--- src/api/auth/auth.repository.ts
+++ src/api/auth/auth.repository.ts
@@ -1,66 +1,45 @@
 import { Injectable } from '@nestjs/common';
-import { BlackList, User, UserProvider } from '@prisma/client';
+import { User } from '@prisma/client';
 import { PrismaService } from 'src/common/module/prisma/prisma.service';
 
 @Injectable()
 export class AuthRepository {
   constructor(private readonly prisma: PrismaService) {}
 
-  async selectUserProviderBySnsId(snsId: string): Promise<UserProvider | null> {
-    return await this.prisma.userProvider.findFirst({
-      where: { snsId },
+  /**
+   * 주어진 snsId를 기반으로 해당 sns id를 가진 사용자를 조회.
+   *
+   * @param {string} snsId - 조회할 sns id
+   * @returns {Promise<User | null>} - 해당 sns id를 가진 사용자가 있으면 User 객체 반환, 없으면 null 반환
+   */
+  async selectUserBySnsId(snsId: string): Promise<User | null> {
+    return await this.prisma.user.findFirst({
+      where: {
+        userProvider: {
+          snsId,
+        },
+      },
     });
   }
 
-  async insertUser(): Promise<User> {
+  /**
+   * 새로운 사용자를 생성하고, 해당 사용자와 연결된 sns provider 정보를 함께 저장.
+   *
+   * @param {string} snsId - 새 사용자의 sns id
+   * @param {number} provider - sns provider (ex: 카카오-0, 애플-1 등)
+   * @returns {Promise<User>} - 생성된 사용자 정보 반환
+   */
+  async insertUser(snsId: string, provider: number): Promise<User> {
     return await this.prisma.user.create({
       data: {
         nickname: 'nickname',
+        userProvider: {
+          create: {
+            snsId,
+            provider,
+          },
+        },
       },
-    });
-  }
-
-  async insertUserProvider(
-    idx: number,
-    provider: number,
-    snsId: string,
-  ): Promise<UserProvider> {
-    return await this.prisma.userProvider.create({
-      data: {
-        idx,
-        provider,
-        snsId,
-      },
-    });
-  }
-
-  async updateUserRefreshTokenByIdx(
-    userIdx: number,
-    refreshToken: string,
-  ): Promise<User> {
-    return await this.prisma.user.update({
-      where: { idx: userIdx },
-      data: { refreshToken: refreshToken },
-    });
-  }
-
-  async selectBlackListByRefreshToken(
-    refreshToken: string,
-  ): Promise<BlackList | null> {
-    return await this.prisma.blackList.findFirst({
-      where: { refreshToken: refreshToken },
-    });
-  }
-
-  async selectUserByIdx(userIdx: number): Promise<User | null> {
-    return await this.prisma.user.findFirst({
-      where: { idx: userIdx },
-    });
-  }
-
-  async insertBlackList(refreshToken: string): Promise<BlackList> {
-    return await this.prisma.blackList.create({
-      data: { refreshToken: refreshToken },
     });
   }
 }
