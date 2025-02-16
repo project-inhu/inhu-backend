422a3069ed356381b8bb3a708c02254e90711884
SuinLee21 <suin7473@naver.com>
2025-02-16 16:29:34 +0900

    feat: Add AuthProviderPipe pipe

---
+++ src/api/auth/pipes/auth-provider.pipe.ts
@@ -0,0 +1,17 @@
+import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
+import { AuthProvider } from '../enum/auth-provider.enum';
+
+@Injectable()
+export class AuthProviderPipe implements PipeTransform<string, AuthProvider> {
+  transform(value: string): AuthProvider {
+    const provider = Object.values(AuthProvider).find(
+      (p) => p.toLowerCase() === value.toLowerCase(),
+    );
+
+    if (!provider) {
+      throw new BadRequestException('Invalid provider');
+    }
+
+    return provider;
+  }
+}
