2f7a3727f6f8794b1fcb6b018bbb5c30df83c955
공실 <kjykjy3314@naver.com>
2025-02-18 02:43:09 +0900

    feat: Add basic module, service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './common/module/prisma/prisma.module';
 import { PlaceModule } from './api/place/place.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, PlaceModule],
+  imports: [PrismaModule, PlaceModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
