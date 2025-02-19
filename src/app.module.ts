34581ccc110653d3e5bfb53543ce1ddb4a7e0ceb
공실 <kjykjy3314@naver.com>
2025-02-19 16:41:13 +0900

    feat: Set up the basic files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './common/module/prisma/prisma.module';
 import { PlaceModule } from './api/place/place.module';
+import { ReviewService } from './api/review/review.service';
+import { ReviewModule } from './api/review/review.module';
 
 @Module({
-  imports: [PrismaModule, PlaceModule],
+  imports: [PrismaModule, PlaceModule, ReviewModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ReviewService],
 })
-export class AppModule { }
+export class AppModule {}
