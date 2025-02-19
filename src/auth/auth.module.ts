0b545c9411dee900a757e71fb9c42b8957fc53a3
공실 <kjykjy3314@naver.com>
2025-02-19 17:59:44 +0900

    feat: Implement auth controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import { AuthService } from './services/auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
