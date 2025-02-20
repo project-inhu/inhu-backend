d05fe56cbe1a9dbf2f1994e77163cb62e8424db4
공실 <kjykjy3314@naver.com>
2025-02-20 17:58:01 +0900

    fix: Add imports and exports

--- src/api/review/review.module.ts
+++ src/api/review/review.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ReviewController } from './review.controller';
+import { ReviewService } from './review.service';
+import { ReviewRepository } from './review.repository';
+import { PrismaModule } from 'src/common/module/prisma/prisma.module';
 
 @Module({
-  controllers: [ReviewController]
+  imports: [PrismaModule],
+  controllers: [ReviewController],
+  providers: [ReviewService, ReviewRepository],
+  exports: [ReviewRepository, ReviewService],
 })
 export class ReviewModule {}
