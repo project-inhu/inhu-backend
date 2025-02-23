b6ae6fcf6268399441774e3e0144753bd3a05df6
SuinLee21 <suin7473@naver.com>
2025-02-23 16:17:45 +0900

    feat: Add getPlaceByPlaceIdx and its dto

---
+++ src/api/place/dto/get-place-detail.dto.ts
@@ -0,0 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsInt, Min } from 'class-validator';
+
+export class GetPlaceByPlaceIdxDto {
+  @ApiProperty({ description: 'place idx number', example: 1 })
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  idx: number;
+
+  @ApiProperty({ description: 'user idx number', example: 1 })
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  userIdx: number;
+}
