7a742ff2955ba2f06eae28bc1b98407ba9463e92
SuinLee21 <suin7473@naver.com>
2025-02-23 14:29:58 +0900

    refactor: Rename method

--- src/api/place/place.repository.ts
+++ src/api/place/place.repository.ts
@@ -1,44 +1,46 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "src/common/module/prisma/prisma.service";
-import { GetAllPlaceDto } from "./place.dto";
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/common/module/prisma/prisma.service';
+import { GetAllPlaceOverviewDto } from './dto/get-all-place-overview.dto';
 
 @Injectable()
 export class PlaceRepository {
-	constructor(private prisma: PrismaService) { }
+  constructor(private prisma: PrismaService) {}
 
-	async getAllPlace(getAllPlaceDto: GetAllPlaceDto) {
-		const { page, userIdx } = getAllPlaceDto;
+  async selectAllPlaceOverview(
+    getAllPlaceOverviewDto: GetAllPlaceOverviewDto,
+  ): Promise<PlaceOverviewQueryResult[]> {
+    const { page, userIdx } = getAllPlaceOverviewDto;
 
-		// keyword를 제외한 place 정보 읽어오기
-		return await this.prisma.place.findMany({
-			skip: page - 1,
-			take: 10,
-			where: {
-				deletedAt: null
-			},
-			select: {
-				idx: true,
-				name: true,
-				address: true,
-				bookmark: {
-					where: {
-						userIdx: userIdx,
-					},
-					select: {
-						idx: true,
-					}
-				},
-				placeImage: {
-					select: {
-						imagePath: true,
-					}
-				},
-				review: {
-					select: {
-						idx: true,
-					}
-				}
-			}
-		})
-	}
+    // keyword를 제외한 place 정보 읽어오기
+    return await this.prisma.place.findMany({
+      skip: page - 1,
+      take: 10,
+      where: {
+        deletedAt: null,
+      },
+      select: {
+        idx: true,
+        name: true,
+        address: true,
+        bookmark: {
+          where: {
+            userIdx: userIdx,
+          },
+          select: {
+            idx: true,
+          },
+        },
+        placeImage: {
+          select: {
+            imagePath: true,
+          },
+        },
+        review: {
+          select: {
+            idx: true,
+          },
+        },
+      },
+    });
+  }
 }
