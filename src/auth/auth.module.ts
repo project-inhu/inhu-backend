1bc1021710656dcda123aadbcf4aeb066b508f9e
SuinLee21 <suin7473@naver.com>
2025-02-23 20:11:52 +0900

    fix: Resolve dependency issues in AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,10 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './services/auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthGuard } from './common/guards/auth.guard';
+import { KakaoStrategy } from './strategies/kakao/kakao.strategy';
+import { LoginTokenService } from './services/login-token.service';
+import { UserModule } from 'src/api/user/user.module';
 
 @Module({
   imports: [
@@ -13,8 +17,10 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         secret: configService.get<string>('JWT_SECRET'),
       }),
     }),
+    UserModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, AuthGuard, KakaoStrategy, LoginTokenService],
+  exports: [AuthService, LoginTokenService]
 })
 export class AuthModule {}
