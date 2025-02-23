83c88d98cd1a55df9e87717eec09fd159889a88b
hj-lab <heeju03180@naver.com>
2025-02-23 18:36:42 +0900

    feat: Add getMyProfile API

---
+++ src/api/user/dto/user-profile-response.dto.ts
@@ -0,0 +1,10 @@
+/**
+ * 내 정보 조회 응답 DTO
+ * - 사용자 프로필 정보(닉네임, 프로필 이미지)를 반환
+ *
+ * @author 조희주
+ */
+export class GetMyProfileResponseDto {
+  profileImagePath: string | null;
+  nickname: string;
+}
