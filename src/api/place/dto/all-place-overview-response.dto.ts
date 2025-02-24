02a36e5b9a006243f41ec1b7e7f2901175f3253e
SuinLee21 <suin7473@naver.com>
2025-02-24 17:48:29 +0900

    feat: Update ApiProperty for swagger

--- src/api/place/dto/all-place-overview-response.dto.ts
+++ src/api/place/dto/all-place-overview-response.dto.ts
@@ -2,6 +2,10 @@ import { ApiProperty } from '@nestjs/swagger';
 import { PlaceOverviewEntity } from '../entity/place-overview.entity';
 
 export class AllPlaceOverviewResponseDto {
-  @ApiProperty({ description: '모든 place entity list' })
+  @ApiProperty({
+    description: '모든 place entity list',
+    type: () => PlaceOverviewEntity,
+    isArray: true,
+  })
   placeOverviewList: PlaceOverviewEntity[];
 }
