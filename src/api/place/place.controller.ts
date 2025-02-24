edb2702a8d198a29fd3f4f4f087caa7db05c18bf
SuinLee21 <suin7473@naver.com>
2025-02-24 17:49:13 +0900

    feat: Add ApiResponse decorator to controller method

--- src/api/place/place.controller.ts
+++ src/api/place/place.controller.ts
@@ -3,11 +3,18 @@ import { PlaceService } from './place.service';
 import { GetAllPlaceOverviewDto } from './dto/get-all-place-overview.dto';
 import { AllPlaceOverviewResponseDto } from './dto/all-place-overview-response.dto';
 import { GetPlaceByPlaceIdxDto } from './dto/get-place-detail.dto';
+import { ApiResponse } from '@nestjs/swagger';
+import { PlaceByPlaceIdxResponseDto } from './dto/place-by-place-idx-response.dto';
 
 @Controller('place')
 export class PlaceController {
   constructor(private placeService: PlaceService) {}
 
+  @ApiResponse({
+    status: 200,
+    description: '성공',
+    type: AllPlaceOverviewResponseDto,
+  })
   @Get('/all')
   async getAllPlaceOverview(
     @Query() getAllPlaceOverviewDto: GetAllPlaceOverviewDto,
@@ -19,14 +26,19 @@ export class PlaceController {
     return { placeOverviewList };
   }
 
+  @ApiResponse({
+    status: 200,
+    description: '성공',
+    type: PlaceByPlaceIdxResponseDto,
+  })
   @Get('/:idx')
   async getPlaceByPlaceIdx(
     @Param() getPlaceByPlaceIdxDto: GetPlaceByPlaceIdxDto,
-  ) {
+  ): Promise<PlaceByPlaceIdxResponseDto> {
     const place = await this.placeService.getPlaceByPlaceIdx(
       getPlaceByPlaceIdxDto,
     );
     console.log(place);
-    return place;
+    return { place };
   }
 }
