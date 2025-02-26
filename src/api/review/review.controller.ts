37f743916ae1a119f6b6e94c22e98df3b00d49e6
공실 <kjykjy3314@naver.com>
2025-02-26 20:19:05 +0900

    refactor: Rename review API route

--- src/api/review/review.controller.ts
+++ src/api/review/review.controller.ts
@@ -2,18 +2,18 @@ import { Controller, Get, Param, Post } from '@nestjs/common';
 import { ReviewService } from './review.service';
 import { GetReviewsByPlaceIdxDto } from './dto/get-reviews-by-place-idx.dto';
 
-@Controller('review')
+@Controller('places')
 export class ReviewController {
   constructor(private reviewService: ReviewService) {}
 
-  @Get(':placeIdx')
+  @Get(':placeIdx/reviews')
   async getReviewsByPlaceIdx(
     @Param('placeIdx') getReviewsByPlaceIdxDto: GetReviewsByPlaceIdxDto,
   ) {
     return this.reviewService.getReviewsByPlaceIdx(getReviewsByPlaceIdxDto);
   }
 
-  @Post(':placeIdx')
+  @Post(':placeIdx/review')
   async createReview(
     @Param('placeIdx') getReviewsByPlaceIdxDto: GetReviewsByPlaceIdxDto,
   ) {}
