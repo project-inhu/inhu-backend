0e248f29b62d6612d79369674b8946fc4609bbcd
공실 <kjykjy3314@naver.com>
2025-02-26 16:32:29 +0900

    chore: Delete useless import

--- src/api/review/review.controller.ts
+++ src/api/review/review.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 import { ReviewService } from './review.service';
+import { GetReviewsByPlaceIdxDto } from './dto/get-reviews-by-place-idx.dto';
 
 @Controller('review')
 export class ReviewController {
   constructor(private reviewService: ReviewService) {}
+
+  @Get(':placeIdx')
+  async getReviewsByPlaceIdx(
+    @Param('placeIdx') getReviewsByPlaceIdxDto: GetReviewsByPlaceIdxDto,
+  ) {
+    return this.reviewService.getReviewsByPlaceIdx(getReviewsByPlaceIdxDto);
+  }
+
+  @Post(':placeIdx')
+  async createReview(
+    @Param('placeIdx') getReviewsByPlaceIdxDto: GetReviewsByPlaceIdxDto,
+  ) {}
 }
