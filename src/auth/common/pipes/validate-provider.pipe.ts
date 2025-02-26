4224d7d655dfc22b804f91b9266e6ec6a57da918
공실 <kjykjy3314@naver.com>
2025-02-26 15:17:50 +0900

    chore: Modify error logic to redirect logic

--- src/auth/common/pipes/validate-provider.pipe.ts
+++ src/auth/common/pipes/validate-provider.pipe.ts
@@ -1,23 +1,23 @@
-import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
+import { Injectable, PipeTransform } from '@nestjs/common';
 import { AuthProvider } from 'src/auth/enums/auth-provider.enum';
 
 /**
  * 소셜 로그인 provider 검증 파이프
  * - 요청된 provider 값이 AuthProvider 열거형에 포함되어 있는지 검증
  *
- * @throws {BadRequestException} 유효하지 않은 provider 값인 경우 예외 발생
+ * 유효하지 않은 provider 값인 경우 null return
  *
  * @author 강정연
  */
 @Injectable()
 export class ValidateProviderPipe implements PipeTransform {
-  transform(value: string): AuthProvider {
+  transform(value: string): AuthProvider | null {
     const provider = Object.values(AuthProvider).find(
       (p) => p.toLowerCase() === value.toLowerCase(),
     );
 
     if (!provider) {
-      throw new BadRequestException('Invalid provider');
+      return null;
     }
 
     return provider;
