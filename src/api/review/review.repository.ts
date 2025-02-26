6012279cff30cee8f4080fdb0ebd3cae74ca7b48
공실 <kjykjy3314@naver.com>
2025-02-26 22:27:09 +0900

    feat: Add review entity and creating entity from prisma function

---
+++ src/api/review/review.repository.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/common/module/prisma/prisma.service';
+import { GetReviewsByPlaceIdxDto } from './dto/get-reviews-by-place-idx.dto';
+import { ReviewEntity } from './entity/review.entity';
+
+@Injectable()
+export class ReviewRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async selectReviewsByPlaceIdx(placeIdx: number) {
+    const reviews = await this.prisma.review.findMany({
+      where: {
+        placeIdx,
+        deletedAt: null,
+      },
+      select: {
+        idx: true,
+        userIdx: true,
+        placeIdx: true,
+        content: true,
+        createdAt: true,
+        reviewImage: {
+          select: {
+            imagePath: true,
+          },
+        },
+        reviewKeywordMapping: {
+          select: {
+            keyword: {
+              select: { content: true },
+            },
+          },
+        },
+      },
+    });
+    return reviews;
+  }
+}
--- src/api/review/review.service.ts
+++ src/api/review/review.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { ReviewRepository } from './review.repository';
+import { GetReviewsByPlaceIdxDto } from './dto/get-reviews-by-place-idx.dto';
+import { ReviewEntity } from './entity/review.entity';
 
 @Injectable()
-export class ReviewService {}
+export class ReviewService {
+  constructor(private readonly reviewRepository: ReviewRepository) {}
+
+  async getReviewsByPlaceIdx(getReviewsByPlaceIdxDto: GetReviewsByPlaceIdxDto) {
+    const { placeIdx } = getReviewsByPlaceIdxDto; // DTO에서 placeIdx 추출
+    return (await this.reviewRepository.selectReviewsByPlaceIdx(placeIdx)).map(
+      ReviewEntity.createEntityFromPrisma,
+    );
+  }
+}
