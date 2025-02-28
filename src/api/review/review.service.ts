36bd0b5ba53d54e963fac7bce24a3ddde61cce9e
공실 <kjykjy3314@naver.com>
2025-02-28 20:06:52 +0900

    feat: Add create review controller

--- src/api/review/review.service.ts
+++ src/api/review/review.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { ReviewRepository } from './review.repository';
 import { GetReviewsByPlaceIdxDto } from './dto/get-reviews-by-place-idx.dto';
 import { ReviewEntity } from './entity/review.entity';
+import { CreateReviewByPlaceIdxDto } from './dto/create-review-by-place-idx.dto';
 
 @Injectable()
 export class ReviewService {
@@ -13,6 +14,18 @@ export class ReviewService {
     const { placeIdx } = getReviewsByPlaceIdxDto;
     return (await this.reviewRepository.selectReviewsByPlaceIdx(placeIdx)).map(
       ReviewEntity.createEntityFromPrisma,
+    );
+  }
+
+  async createReviewByPlaceIdx(
+    createReviewByPlaceIdxDto: CreateReviewByPlaceIdxDto,
+  ) {
+    const { placeIdx, content, keywordIdx } = createReviewByPlaceIdxDto;
+
+    const review = await this.reviewRepository.createReviewByPlaceIdx(
+      placeIdx,
+      content,
+      1,
     );
   }
 }
