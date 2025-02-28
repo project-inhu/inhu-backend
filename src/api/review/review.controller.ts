955cb59a14c1b736d06f11f0e91dad305e099874
공실 <kjykjy3314@naver.com>
2025-02-28 17:50:17 +0900

    fix: Add type

--- src/api/review/review.controller.ts
+++ src/api/review/review.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { ReviewService } from './review.service';
 import { GetReviewsByPlaceIdxDto } from './dto/get-reviews-by-place-idx.dto';
+import { CreateReviewByPlaceIdxDto } from './dto/create-review-by-place-idx.dto';
+import { ReviewEntity } from './entity/review.entity';
 
 @Controller('places')
 export class ReviewController {
@@ -9,12 +11,12 @@ export class ReviewController {
   @Get(':placeIdx/reviews')
   async getReviewsByPlaceIdx(
     @Param() getReviewsByPlaceIdxDto: GetReviewsByPlaceIdxDto,
-  ) {
+  ): Promise<ReviewEntity[]> {
     return this.reviewService.getReviewsByPlaceIdx(getReviewsByPlaceIdxDto);
   }
 
   @Post(':placeIdx/review')
-  async createReview(
-    @Param('placeIdx') getReviewsByPlaceIdxDto: GetReviewsByPlaceIdxDto,
+  async createReviewByPlaceIdx(
+    @Param('placeIdx') createReviewByPlaceIdxDto: CreateReviewByPlaceIdxDto,
   ) {}
 }
