c8a69570a51d93348d7fb21cf99e3b1c4dd1c0b8
SuinLee21 <suin7473@naver.com>
2025-03-01 11:19:58 +0900

    refactor: Update swagger documantation

--- src/api/place/dto/place-by-place-idx-response.dto.ts
+++ src/api/place/dto/place-by-place-idx-response.dto.ts
@@ -1,10 +1,5 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { PlaceEntity } from '../entity/place.entity';
 
 export class PlaceByPlaceIdxResponseDto {
-  @ApiProperty({
-    description: '모든 place entity list',
-    type: () => PlaceEntity,
-  })
   place: PlaceEntity | null;
 }
