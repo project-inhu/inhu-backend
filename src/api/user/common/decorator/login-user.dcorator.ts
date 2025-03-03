aa40d73cd4f7702d975da121fb517288faf323d2
hj-lab <heeju03180@naver.com>
2025-03-03 21:42:44 +0900

    feat: Add LoginUser decorator

---
+++ src/api/user/common/decorator/login-user.dcorator.ts
@@ -0,0 +1,13 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+/**
+ * 로그인된 사용자 정보를 가져오는 데코레이터
+ *
+ * @author 조희주
+ */
+export const LoginUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.user;
+  },
+);
