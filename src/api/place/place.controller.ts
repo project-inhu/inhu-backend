23bec1b624143fb797f670507bf98590a00bfdb5
SuinLee21 <suin7473@naver.com>
2025-03-05 20:56:24 +0900

    docs: Add comments to place controller methods

--- src/api/place/place.controller.ts
+++ src/api/place/place.controller.ts
@@ -10,6 +10,11 @@ import { PlaceByPlaceIdxResponseDto } from './dto/place-by-place-idx-response.dt
 export class PlaceController {
   constructor(private placeService: PlaceService) {}
 
+  /**
+   * 모든 place 개요 가져오기
+   *
+   * @author 이수인
+   */
   @ApiOkResponse({ type: AllPlaceOverviewResponseDto })
   @Get('/all')
   async getAllPlaceOverview(
@@ -22,6 +27,11 @@ export class PlaceController {
     return { placeOverviewList };
   }
 
+  /**
+   * place 관련 모든 정보 가져오기
+   *
+   * @author 이수인
+   */
   @ApiOkResponse({ type: PlaceByPlaceIdxResponseDto })
   @Get('/:idx')
   async getPlaceByIdx(
