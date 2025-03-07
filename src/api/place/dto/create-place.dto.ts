568930d284bb37854bc9a9c05a6b160d2731e135
SuinLee21 <suin7473@naver.com>
2025-03-08 00:38:47 +0900

    feat: Add createPlace method

---
+++ src/api/place/dto/create-place.dto.ts
@@ -0,0 +1,28 @@
+import { IsArray, IsDecimal, IsString, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+import { WeekScheduleDto } from './week-schedule.dto';
+
+export class CreatePlaceDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  tel: string;
+
+  @IsString()
+  address: string;
+
+  @IsDecimal()
+  addressX: string;
+
+  @IsDecimal()
+  addressY: string;
+
+  @ValidateNested()
+  @Type(() => WeekScheduleDto)
+  week: WeekScheduleDto;
+
+  @IsArray()
+  @IsString({ each: true })
+  placeImage: string[];
+}
