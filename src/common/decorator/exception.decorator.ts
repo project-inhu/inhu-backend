6ee9c9438be42a94389001eac81847e4535140d6
공실 <kjykjy3314@naver.com>
2025-03-16 15:14:01 +0900

    feat: Add authentication decorator and exception response decorator

---
+++ src/common/decorator/exception.decorator.ts
@@ -0,0 +1,10 @@
+import { applyDecorators } from '@nestjs/common';
+import { ApiResponse } from '@nestjs/swagger';
+
+/**
+ * 상태코드와 메시지를 반환하는 예외 응답 데코레이터
+ *
+ * @author 강정연
+ */
+export const Execption = (status: number, message: string) =>
+  applyDecorators(ApiResponse({ status, description: message }));
