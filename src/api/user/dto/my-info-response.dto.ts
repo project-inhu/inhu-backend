68759979f762a5bec565fe3b9785fc3a65bbf296
hj-lab <heeju03180@naver.com>
2025-03-16 16:04:09 +0900

    refactor: Remove redundant class validator in MyInfoResponseDto

--- src/api/user/dto/my-info-response.dto.ts
+++ src/api/user/dto/my-info-response.dto.ts
@@ -1,5 +1,4 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsDate, IsInt, IsOptional, IsString } from 'class-validator';
 
 /**
  * 내 정보 응답 DTO
@@ -8,11 +7,9 @@ import { IsDate, IsInt, IsOptional, IsString } from 'class-validator';
  */
 export class MyInfoResponseDto {
   @ApiProperty({ description: '사용자 고유 idx', example: 1 })
-  @IsInt()
   idx: number;
 
   @ApiProperty({ description: '사용자 닉네임', example: 'heeju' })
-  @IsString()
   nickname: string;
 
   @ApiProperty({
@@ -20,15 +17,12 @@ export class MyInfoResponseDto {
     example: 'https://inhu.s3.ap-northeast-2.amazonaws.com/user123/profile.jpg',
     nullable: true,
   })
-  @IsOptional()
-  @IsString()
   profileImagePath: string | null;
 
   @ApiProperty({
     description: '계정 생성일',
     example: '2025-03-07T08:50:21.451Z',
   })
-  @IsDate()
   createdAt: Date;
 
   @ApiProperty({
@@ -36,7 +30,5 @@ export class MyInfoResponseDto {
     example: '2025-03-10T08:50:21.451Z',
     nullable: true,
   })
-  @IsOptional()
-  @IsDate()
   deletedAt?: Date | null;
 }
