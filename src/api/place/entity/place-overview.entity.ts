7f2741cfc3f23a22fd78a75a208e024a72ae827e
SuinLee21 <suin7473@naver.com>
2025-03-17 22:23:12 +0900

    chore: Remove ApiPropery and change some parameter type

--- src/api/place/entity/place-overview.entity.ts
+++ src/api/place/entity/place-overview.entity.ts
@@ -1,5 +1,6 @@
 import { PickType } from '@nestjs/swagger';
 import { PlaceEntity } from './place.entity';
+import { PlaceOverviewSelectField } from '../type/place-overview-select-field.type';
 
 export class PlaceOverviewEntity extends PickType(PlaceEntity, [
   'idx',
@@ -15,16 +16,15 @@ export class PlaceOverviewEntity extends PickType(PlaceEntity, [
   }
 
   static createEntityFromPrisma(
-    place: PlaceOverviewQueryResult,
+    place: PlaceOverviewSelectField,
   ): PlaceOverviewEntity {
     return new PlaceOverviewEntity({
       idx: place.idx,
       name: place.name,
       address: place.address,
       reviewCount: place.review.length,
-      bookmark: place.bookmark.length > 0 ? true : false,
-      // keyword: [],
-      imagePathList: place.placeImage.map((path) => path.imagePath ?? ''),
+      bookmark: place.bookmark?.length ? true : false,
+      imagePathList: place.placeImage.map((image) => image.path ?? ''),
     });
   }
 }
