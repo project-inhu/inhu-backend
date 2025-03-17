882f9ac8f7da1be89893ef3ba149eb25f849ff19
SuinLee21 <suin7473@naver.com>
2025-03-17 22:19:05 +0900

    refactor: Remove query result interface and use Prisma PlaceGetPayload instead

---
+++ src/api/place/type/place-overview-select-field.type.ts
@@ -0,0 +1,42 @@
+import { Prisma } from '@prisma/client';
+
+/**
+ * Prisma에서 조회된 place overview 데이터
+ *
+ * `PlaceOverviewEntity`로 변환되기 전의 원시 데이터
+ *
+ * @author 이수인
+ */
+const PLACE_OVERVIEW_SELECT_FIELD = Prisma.validator<Prisma.PlaceDefaultArgs>()(
+  {
+    select: {
+      idx: true,
+      name: true,
+      address: true,
+      bookmark: {
+        select: {
+          idx: true,
+        },
+      },
+      placeImage: {
+        select: {
+          path: true,
+        },
+      },
+      review: {
+        select: {
+          idx: true,
+        },
+      },
+    },
+  },
+);
+
+/**
+ * `PLACE_OVERVIEW_SELECT_FIELD`의 Prisma 반환 타입
+ *
+ * @author 이수인
+ */
+export type PlaceOverviewSelectField = Prisma.PlaceGetPayload<
+  typeof PLACE_OVERVIEW_SELECT_FIELD
+>;
