692ac75b4f0bfa844e2e80b762641a3354319673
공실 <kjykjy3314@naver.com>
2025-04-12 22:48:33 +0900

    refactor: Change seed default values to fixed values and apply faker

--- test/common/seed/utils/random-utils.ts
+++ test/common/seed/utils/random-utils.ts
@@ -1,36 +1,24 @@
+import { faker } from '@faker-js/faker/.';
 import { PrismaService } from 'src/common/module/prisma/prisma.service';
 
 export function getRandomInt(min: number, max: number): number {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-export function getRandomSubset<T>(array: T[], count: number): T[] {
-  const shuffled = [...array].sort(() => Math.random() - 0.5);
-  return shuffled.slice(0, count);
+  return faker.number.int({ min, max });
 }
 
 export function getRandomImagePathList(): string[] {
-  const imagePathCandidateList = [
-    'images/sample1.jpg',
-    'images/sample2.jpg',
-    'images/sample3.jpg',
-    'images/sample4.jpg',
-    'images/sample5.jpg',
-  ];
-  const count = getRandomInt(1, imagePathCandidateList.length);
-  return getRandomSubset(imagePathCandidateList, count);
+  const count = getRandomInt(1, 5);
+  return Array.from(
+    { length: count },
+    (_, i) => `images/${faker.word.adjective()}_${i + 1}.jpg`,
+  );
 }
 
-export function getRandomContent(): string {
-  const randomPhrases = [
-    'The place is great, I had an amazing time!',
-    'I loved the food, but the service could be better.',
-    'Very quiet and peaceful place, perfect for a weekend retreat.',
-    'Great ambiance, but the prices are a bit high.',
-    'Highly recommend, will visit again!',
-    'Good place but could improve on cleanliness.',
-  ];
-  return randomPhrases[getRandomInt(0, randomPhrases.length - 1)];
+export function getRandomContent(min = 3, max = 400): string {
+  let result = '';
+  while (result.length < min) {
+    result = faker.lorem.sentences(5).slice(0, max).trim();
+  }
+  return result;
 }
 
 export async function getRandomKeywordPairList(
@@ -38,7 +26,7 @@ export async function getRandomKeywordPairList(
 ): Promise<{ idx: number; content: string }[]> {
   const keywordList = await prisma.keyword.findMany();
   const count = getRandomInt(1, 3);
-  const selectedKeywords = getRandomSubset(keywordList, count);
+  const selectedKeywords = faker.helpers.arrayElements(keywordList, count);
   selectedKeywords.sort((a, b) => a.idx - b.idx);
 
   return selectedKeywords.map((keyword) => ({
