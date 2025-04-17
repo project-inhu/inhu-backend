db86a0d0db5be737446dda3cacdea273397cec21
공실 <kjykjy3314@naver.com>
2025-04-17 16:59:46 +0900

    feat: Add creating bookmark logic

--- src/api/bookmark/bookmark.repository.ts
+++ src/api/bookmark/bookmark.repository.ts
@@ -1,7 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { Bookmark } from '@prisma/client';
 import { PrismaService } from 'src/common/module/prisma/prisma.service';
+import { BookmarkSelectField } from './type/bookmark-select-field';
 
 @Injectable()
-export class ReviewRepository {
+export class BookmarkRepository {
   constructor(private readonly prisma: PrismaService) {}
+
+  async createBookmarkByPlaceIdx(
+    placeIdx: number,
+    userIdx: number,
+  ): Promise<Bookmark> {
+    return await this.prisma.bookmark.create({
+      data: {
+        placeIdx,
+        userIdx,
+      },
+    });
+  }
+
+  async selectBookmarkByBookmarkIdx(
+    bookmarkIdx: number,
+  ): Promise<BookmarkSelectField | null> {
+    return await this.prisma.bookmark.findUnique({
+      where: {
+        idx: bookmarkIdx,
+        deletedAt: null,
+      },
+      select: {
+        idx: true,
+        userIdx: true,
+        placeIdx: true,
+        createdAt: true,
+      },
+    });
+  }
 }
