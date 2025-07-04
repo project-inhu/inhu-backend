9b1b5b8d45cdb78398c8186c3725336bcc68f1e5
hj-lab <heeju03180@naver.com>
2025-07-04 18:48:31 +0900

    feat: Initialize S3Client in S3Service

--- src/common/s3/s3.service.ts
+++ src/common/s3/s3.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { S3Client } from '@aws-sdk/client-s3';
 
 @Injectable()
-export class S3Service {}
+export class S3Service {
+  private readonly s3Client: S3Client;
+  private readonly bucketName: string;
+  private readonly region: string;
+
+  constructor(private readonly configService: ConfigService) {
+    const region = this.configService.get('AWS_REGION');
+    const bucketName = this.configService.get('AWS_S3_BUCKET_NAME');
+
+    if (!region || !bucketName) {
+      throw new InternalServerErrorException(
+        'S3 environment variables are not configured.',
+      );
+    }
+
+    this.region = region;
+    this.bucketName = bucketName;
+
+    this.s3Client = new S3Client({
+      region: this.region,
+    });
+  }
+}
