3602c43108c8dc0e5cd50e867b1f510a44db4445
공실 <kjykjy3314@naver.com>
2025-07-04 20:47:12 +0900

    feat: Add keyword entity

---
+++ src/api/keyword/entity/keyword.entity.ts
@@ -0,0 +1,23 @@
+export class ReviewKeywordEntity {
+  /**
+   * keyword idx
+   * @example 1
+   */
+  idx: number;
+
+  /**
+   * keyword 내용
+   * @example "인테리어가 예뻐요"
+   */
+  content: string;
+
+  /**
+   * 키워드 생성 날짜
+   * @example "2024-03-11T12:34:56.789Z"
+   */
+  createdAt: Date;
+
+  constructor(data: ReviewKeywordEntity) {
+    Object.assign(this, data);
+  }
+}
