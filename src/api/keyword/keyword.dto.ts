dbc6366a4bacb42b36e9027e48198a4eead93465
공실 <kjykjy3314@naver.com>
2025-07-04 20:54:30 +0900

    refactor: Rename keywordEntity and keywordDto

--- src/api/keyword/keyword.dto.ts
+++ src/api/keyword/keyword.dto.ts
@@ -1,19 +1,22 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsInt, Min } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, Min } from 'class-validator';
 
-export class KeywordEntity {
-    @ApiProperty({ description: 'place idx', example: 1 })
-    @IsInt()
-    @Min(1)
-    placeIdx: number;
+export class KeywordDto {
+  @ApiProperty({ description: 'place idx', example: 1 })
+  @IsInt()
+  @Min(1)
+  placeIdx: number;
 
-    @ApiProperty({ description: 'keyword idx', example: 1 })
-    @IsInt()
-    @Min(1)
-    keywordIdx: number;
+  @ApiProperty({ description: 'keyword idx', example: 1 })
+  @IsInt()
+  @Min(1)
+  keywordIdx: number;
 
-    @ApiProperty({ description: '특정 장소에서 특정 keyword가 사용된 개수', example: 1 })
-    @IsInt()
-    @Min(0)
-    keywordCount: number;
+  @ApiProperty({
+    description: '특정 장소에서 특정 keyword가 사용된 개수',
+    example: 1,
+  })
+  @IsInt()
+  @Min(0)
+  keywordCount: number;
 }
