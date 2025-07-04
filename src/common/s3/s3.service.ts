d9b31fd8f7030d8bb1964a06427d5a23b79da952
hj-lab <heeju03180@naver.com>
2025-07-04 21:03:06 +0900

    feat: Add uploadFile method

--- src/common/s3/s3.service.ts
+++ src/common/s3/s3.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { S3Client } from '@aws-sdk/client-s3';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class S3Service {
@@ -24,5 +25,19 @@ export class S3Service {
     this.s3Client = new S3Client({
       region: this.region,
     });
+  }
+
+  async uploadFile(file: Express.Multer.File): Promise<string> {
+    const key = `${uuid()}${file.originalname}`;
+
+    const command = new PutObjectCommand({
+      Bucket: this.bucketName,
+      Key: key,
+      Body: file.buffer,
+      ContentType: file.mimetype,
+    });
+
+    await this.s3Client.send(command);
+    return key;
   }
 }
