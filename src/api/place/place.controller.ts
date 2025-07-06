159f741b687fb669a87ec8099211984eace3bf91
공실 <kjykjy3314@naver.com>
2025-07-07 01:00:43 +0900

    fix: Delete console log for test

--- src/api/place/place.controller.ts
+++ src/api/place/place.controller.ts
@@ -19,7 +19,6 @@ export class PlaceController {
     @Query() getAllPlaceOverviewDto: GetAllPlaceOverviewDto,
     @User('idx') userIdx?: number,
   ): Promise<PlaceOverviewEntity[]> {
-    console.log('[DEBUG] getAllPlaceOverviewDto:', getAllPlaceOverviewDto);
     return await this.placeService.getAllPlaceOverview(
       getAllPlaceOverviewDto.page,
       getAllPlaceOverviewDto.orderBy,
