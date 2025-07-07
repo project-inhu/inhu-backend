60069b9e6a38e120cace8bf211824d96023e457a
SuinLee21 <suin7473@naver.com>
2025-07-08 02:51:49 +0900

    chore: Add apple login page for test apple login

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as path from 'path';
+import { Request, Response } from 'express';
+import { AuthGuard } from './auth/common/guards/auth.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/test')
+  test(@Req() req: Request, @Res() res: Response) {
+    const filePath = path.join(
+      '/home/ubuntu/inhu-backend/inhu-backend/public',
+      'testKakao.html',
+    );
+    return res.sendFile(filePath);
   }
 }
