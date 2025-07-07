d43f38d68ed00044638d52795d4f9928ecac5224
공실 <kjykjy3314@naver.com>
2025-07-07 23:46:01 +0900

    feat: Add picked-place service

--- src/api/picked-place/picked-place.service.ts
+++ src/api/picked-place/picked-place.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PickedPlaceRepository } from './picked-place.repository';
 
 @Injectable()
-export class PickedPlaceService {}
+export class PickedPlaceService {
+  constructor(private readonly pickedPlaceRepository: PickedPlaceRepository) {}
+
+  async getAllPickedPlace(page: number, userIdx?: number) {
+    return await this.pickedPlaceRepository.selectAllPickedPlace(page, userIdx);
+  }
+}
