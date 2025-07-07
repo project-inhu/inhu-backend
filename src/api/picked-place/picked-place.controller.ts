3a1a7a14303cc663a81253a8cab9d731824a96ec
공실 <kjykjy3314@naver.com>
2025-07-07 23:48:19 +0900

    feat: Add picked-place controller

--- src/api/picked-place/picked-place.controller.ts
+++ src/api/picked-place/picked-place.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Query } from '@nestjs/common';
+import { PickedPlaceService } from './picked-place.service';
+import { GetAllPickedPlaceDto } from './dto/get-all-picked-place.dto';
+import { User } from 'src/common/decorator/user.decorator';
 
 @Controller('picked-place')
-export class PickedPlaceController {}
+export class PickedPlaceController {
+  constructor(private pickedPlaceService: PickedPlaceService) {}
+
+  async getAllPickedPlace(
+    @Query() getAllPickedPlaceDto: GetAllPickedPlaceDto,
+    @User('idx') userIdx?: number,
+  ) {
+    return await this.pickedPlaceService.getAllPickedPlace(
+      getAllPickedPlaceDto.page,
+      userIdx,
+    );
+  }
+}
