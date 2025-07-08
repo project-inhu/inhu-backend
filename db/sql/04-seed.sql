61bdea78750ee6acaef6ab17cc4ed00b3250abca
공실 <kjykjy3314@naver.com>
2025-07-09 01:31:51 +0900

    fix: Modify the image_paths

--- db/sql/04-seed.sql
+++ db/sql/04-seed.sql
@@ -1,21 +1,22 @@
+INSERT INTO user_tb (nickname, profile_image_path) VALUES
+('맛집탐방가', '/profile/ddccb241-5fc2-4aca-9f6f-89a2b32edd68-puppy.jpg');
 INSERT INTO user_tb (nickname) VALUES
-('맛집탐방가'),
 ('인덕이');
 
 INSERT INTO place_tb (name, tel, address, address_x, address_y) VALUES
 ('스타벅스 인하대점', '1522-3232', '인천 미추홀구 인하로59', 126.650892, 37.452601);
 INSERT INTO menu_tb (place_idx, name, content, price, image_path) VALUES 
-(1, '아이스 아메리카노', '시원하고 깔끔한 맛', 4500, '/menu/a1b2c3d4-e5f6-7890-1234-567890abcdef-iced_americano.jpg'),
-(1, '디카페인 아메리카노', '카페인 부담 없이 즐기는 커피', 4800, '/menu/b2c3d4e5-f6a7-8901-2345-67890abcdef0-decaf_americano.jpg'),
-(1, '카라멜 마끼아또', '달콤한 카라멜과 부드러운 우유의 조화', 5900, '/menu/c3d4e5f6-a7b8-9012-3456-7890abcdef01-caramel_macchiato.jpg'),
-(1, '돌체 콜드 브루', '연유의 달콤함과 콜드 브루의 깊은 풍미', 6000, '/menu/d4e5f6a7-b8c9-0123-4567-890abcdef012-dolce_cold_brew.jpg'),
-(1, '자몽 허니 블랙 티', '자몽과 꿀의 상큼달콤한 조화', 5700, '/menu/e5f6a7b8-c9d0-1234-5678-90abcdef0123-grapefruit_honey_tea.jpg'),
-(1, '딸기 아사이 레모네이드 리프레셔', '딸기와 아사이베리의 상큼한 맛', 5900, '/menu/f6a7b8c9-d0e1-2345-6789-0abcdef01234-strawberry_refresher.jpg'),
-(1, '제주 말차 라떼', '제주산 말차의 진하고 쌉싸름한 맛', 6100, '/menu/a7b8c9d0-e1f2-3456-7890-abcdef012345-jeju_matcha_latte.jpg'),
-(1, '클래식 스콘', '버터 풍미 가득한 담백한 스콘', 3500, '/menu/b8c9d0e1-f2a3-4567-8901-bcdef0123456-classic_scone.jpg'),
-(1, '블루베리 베이글', '블루베리가 콕콕 박힌 쫄깃한 베이글', 3000, '/menu/c9d0e1f2-a3b4-5678-9012-cdef01234567-blueberry_bagel.jpg'),
-(1, '부드러운 생크림 카스텔라', '촉촉한 카스텔라와 부드러운 생크림', 4500, '/menu/d0e1f2a3-b4c5-6789-0123-def012345678-cream_castella.jpg'),
-(1, '바닐라 크림 콜드 브루', '바닐라 크림의 부드러움과 콜드 브루의 만남', 5800, '/menu/e1f2a3b4-c5d6-7890-1234-ef0123456789-vanilla_cream_cold_brew.jpg');
+(1, '아이스 아메리카노', '시원하고 깔끔한 맛', 4500, '/menu/41ee298f-7745-43cd-b81b-374a0e692fc9-candies.jpg'),
+(1, '디카페인 아메리카노', '카페인 부담 없이 즐기는 커피', 4800, '/menu/41ee298f-7745-43cd-b81b-374a0e692fc9-candies.jpg'),
+(1, '카라멜 마끼아또', '달콤한 카라멜과 부드러운 우유의 조화', 5900, '/menu/41ee298f-7745-43cd-b81b-374a0e692fc9-candies.jpg'),
+(1, '돌체 콜드 브루', '연유의 달콤함과 콜드 브루의 깊은 풍미', 6000, '/menu/41ee298f-7745-43cd-b81b-374a0e692fc9-candies.jpg'),
+(1, '자몽 허니 블랙 티', '자몽과 꿀의 상큼달콤한 조화', 5700, '/menu/41ee298f-7745-43cd-b81b-374a0e692fc9-candies.jpg'),
+(1, '딸기 아사이 레모네이드 리프레셔', '딸기와 아사이베리의 상큼한 맛', 5900, '/menu/41ee298f-7745-43cd-b81b-374a0e692fc9-candies.jpg'),
+(1, '제주 말차 라떼', '제주산 말차의 진하고 쌉싸름한 맛', 6100, '/menu/41ee298f-7745-43cd-b81b-374a0e692fc9-candies.jpg'),
+(1, '클래식 스콘', '버터 풍미 가득한 담백한 스콘', 3500, '/menu/41ee298f-7745-43cd-b81b-374a0e692fc9-candies.jpg'),
+(1, '블루베리 베이글', '블루베리가 콕콕 박힌 쫄깃한 베이글', 3000, '/menu/41ee298f-7745-43cd-b81b-374a0e692fc9-candies.jpg'),
+(1, '부드러운 생크림 카스텔라', '촉촉한 카스텔라와 부드러운 생크림', 4500, '/menu/41ee298f-7745-43cd-b81b-374a0e692fc9-candies.jpg'),
+(1, '바닐라 크림 콜드 브루', '바닐라 크림의 부드러움과 콜드 브루의 만남', 5800,'/menu/41ee298f-7745-43cd-b81b-374a0e692fc9-candies.jpg');
 INSERT INTO operating_day_tb (place_idx, day) VALUES
 (1, 'mon'),
 (1, 'tue'),
@@ -33,7 +34,7 @@ INSERT INTO operating_hour_tb (operating_day_idx, start_at, end_at) VALUES
 (6, '08:00:00', '22:00:00'),
 (7, '08:00:00', '22:00:00');
 INSERT INTO place_image_tb (place_idx, image_path) VALUES
-(1, 'place/f9c2e36f-8e99-4b18-b3e8-7cd327682f94_20240706_124512.jpg');
+(1, '/place/bakery.jpg');
 INSERT INTO place_type_mapping_tb (place_idx, place_type_idx) VALUES 
 (1, 1);
 
@@ -70,7 +71,7 @@ INSERT INTO break_time_tb (operating_hour_idx, start_at, end_at) VALUES
 (14, '10:00:00', '10:30:00'), 
 (15, '17:00:00', '17:30:00');
 INSERT INTO place_image_tb (place_idx, image_path) VALUES 
-(2, 'place/7e4a0a69-60d2-4d90-a1aa-70ef8e40816d_20240706_124512.jpg');
+(2, '/place/bakery.jpg');
 INSERT INTO place_type_mapping_tb (place_idx, place_type_idx) VALUES 
 (2, 2);
 
@@ -96,9 +97,8 @@ INSERT INTO operating_hour_tb (operating_day_idx, start_at, end_at) VALUES
 (19, '11:00:00', '23:00:00'), 
 (20, '11:00:00', '23:00:00');
 INSERT INTO place_image_tb (place_idx, image_path) VALUES
-(3, 'place/c2b1e0f0-1234-5678-9abc-def012345678_20250706_120000.jpg');
+(3, '/place/big_volume_mountain.jpg');
 INSERT INTO place_type_mapping_tb (place_idx, place_type_idx) VALUES (3, 2); 
-
 INSERT INTO place_tb (name, tel, address, address_x, address_y) VALUES
 ('새로운 카페이름', '070-9876-5432', '인천 미추홀구 학익동 123', 126.660000, 37.450000);
 INSERT INTO menu_tb (place_idx, name, content, price) VALUES
@@ -116,7 +116,7 @@ INSERT INTO operating_hour_tb (operating_day_idx, start_at, end_at) VALUES
 (24, '09:00:00', '18:00:00'), 
 (25, '09:00:00', '18:00:00');
 INSERT INTO place_image_tb (place_idx, image_path) VALUES
-(4, 'place/d3e4f5g6-7890-abcd-ef01-234567890abc_20250706_120100.jpg');
+(4, '/place/big_volume_mountain.jpg');
 INSERT INTO place_type_mapping_tb (place_idx, place_type_idx) VALUES (4, 1);
 
 INSERT INTO place_tb (name, tel, address, address_x, address_y) VALUES
@@ -139,7 +139,7 @@ INSERT INTO operating_hour_tb (operating_day_idx, start_at, end_at) VALUES
 (30, '10:00:00', '21:00:00'), 
 (31, '10:00:00', '21:00:00');
 INSERT INTO place_image_tb (place_idx, image_path) VALUES
-(5, 'place/e4f5g6h7-8901-2345-6789-0abcdef01234_20250706_120200.jpg');
+(5, '/place/big_volume_mountain.jpg');
 INSERT INTO place_type_mapping_tb (place_idx, place_type_idx) VALUES (5, 2); 
 
 INSERT INTO place_tb (name, tel, address, address_x, address_y) VALUES
@@ -153,7 +153,7 @@ INSERT INTO break_time_tb (operating_hour_idx, start_at, end_at) VALUES
 INSERT INTO menu_tb (place_idx, name, price) VALUES 
 (6, '김치찌개', 8500), (6, '된장찌개', 8500);
 INSERT INTO place_image_tb (place_idx, image_path) VALUES 
-(6, 'place/6a7b8c9d-1234-abcd-efgh-ijklmnopqrst.jpg');
+(6, '/place/bakery.jpg');
 INSERT INTO place_type_mapping_tb (place_idx, place_type_idx) VALUES (6, 2);
 
 INSERT INTO place_tb (name, tel, address, address_x, address_y) VALUES
@@ -165,7 +165,7 @@ INSERT INTO operating_hour_tb (operating_day_idx, start_at, end_at) VALUES
 INSERT INTO menu_tb (place_idx, name, content, price) VALUES 
 (7, '딸기빙수', '신선한 딸기가 듬뿍', 13000), (7, '치즈케이크', '꾸덕한 뉴욕 치즈케이크', 6500);
 INSERT INTO place_image_tb (place_idx, image_path) VALUES 
-(7, 'place/7b8c9d0e-5678-efgh-ijkl-mnopqrstuv.jpg');
+(7, '/place/bakery.jpg');
 INSERT INTO place_type_mapping_tb (place_idx, place_type_idx) VALUES (7, 1);
 
 INSERT INTO place_tb (name, tel, address, address_x, address_y) VALUES
@@ -177,7 +177,7 @@ INSERT INTO operating_hour_tb (operating_day_idx, start_at, end_at) VALUES
 INSERT INTO menu_tb (place_idx, name, price) VALUES 
 (8, '가츠동', 9000), (8, '사케동', 12000);
 INSERT INTO place_image_tb (place_idx, image_path) VALUES 
-(8, 'place/8c9d0e1f-9012-ijkl-mnop-qrstuvwxy.jpg');
+(8, '/place/bakery.jpg');
 INSERT INTO place_type_mapping_tb (place_idx, place_type_idx) VALUES (8, 2);
 
 INSERT INTO place_tb (name, tel, address, address_x, address_y) VALUES
@@ -189,7 +189,7 @@ INSERT INTO operating_hour_tb (operating_day_idx, start_at, end_at) VALUES
 INSERT INTO menu_tb (place_idx, name, price) VALUES 
 (9, '페일에일', 7000), (9, '치킨 플래터', 24000);
 INSERT INTO place_image_tb (place_idx, image_path) VALUES 
-(9, 'place/9d0e1f2g-3456-mnop-qrst-uvwxyz012.jpg');
+(9, '/place/bakery.jpg');
 INSERT INTO place_type_mapping_tb (place_idx, place_type_idx) VALUES (9, 2);
 
 INSERT INTO place_tb (name, tel, address, address_x, address_y) VALUES
@@ -201,7 +201,7 @@ INSERT INTO operating_hour_tb (operating_day_idx, start_at, end_at) VALUES
 INSERT INTO menu_tb (place_idx, name, price) VALUES 
 (10, '4시간 이용권', 6000);
 INSERT INTO place_image_tb (place_idx, image_path) VALUES 
-(10, 'place/0e1f2g3h-7890-qrst-uvwx-yz0123456.jpg');
+(10, '/place/bakery.jpg');
 INSERT INTO place_type_mapping_tb (place_idx, place_type_idx) VALUES (10, 1);
 
 INSERT INTO place_tb (name, tel, address, address_x, address_y) VALUES
@@ -213,7 +213,7 @@ INSERT INTO operating_hour_tb (operating_day_idx, start_at, end_at) VALUES
 INSERT INTO menu_tb (place_idx, name, price) VALUES 
 (11, '소금빵', 3000), (11, '앙버터', 4500);
 INSERT INTO place_image_tb (place_idx, image_path) VALUES 
-(11, 'place/1f2g3h4i-1234-uvwx-yz01-234567890.jpg');
+(11, '/place/big_volume_mountain.jpg');
 INSERT INTO place_type_mapping_tb (place_idx, place_type_idx) VALUES (11, 2);
 
 INSERT INTO place_tb (name, tel, address, address_x, address_y) VALUES
@@ -225,7 +225,7 @@ INSERT INTO operating_hour_tb (operating_day_idx, start_at, end_at) VALUES
 INSERT INTO menu_tb (place_idx, name, price) VALUES 
 (12, '싸이버거 세트', 7200);
 INSERT INTO place_image_tb (place_idx, image_path) VALUES 
-(12, 'place/2g3h4i5j-5678-yz01-2345-67890abcd.jpg');
+(12, '/place/big_volume_mountain.jpg');
 INSERT INTO place_type_mapping_tb (place_idx, place_type_idx) VALUES (12, 2);
 
 
@@ -250,15 +250,15 @@ INSERT INTO review_tb (user_idx, place_idx, content) VALUES
 (2, 12, '역시 믿고 먹는 싸이버거입니다.');
 
 INSERT INTO review_image_tb (review_idx, image_path) VALUES
-(2, 'review/f7c873c2-4fc6-4a8d-9b8e-85d8a51f56b3-.jpg'),
-(2, 'review/b631f9d2-35d1-4b5f-a97f-52f4f41326e3_20240706_124512.jpg'),
-(3, 'review/baebea0a-9110-455f-84f6-5608f8978c47_20240706_124512.jpg'),
-(4, 'review/e1a2b3c4-d5e6-f7a8-9b0c-1d2e3f4a5b6c_20250706225300.jpg'),
-(4, 'review/f8e9d0c1-b2a3-4b5c-6d7e-8f9a0b1c2d3e_20250706225301.jpg'),
-(8, 'review/g9h0i1j2-k3l4-m5n6-o7p8-q9r0s1t2u3v4_20250706225302.jpg'),
-(9, 'review/new_image_1.jpg'),
-(11, 'review/new_image_2.jpg'),
-(13, 'review/new_image_3.jpg');
+(2, '/review/b631f9d2-35d1-4b5f-a97f-52f4f41326e3_20240706_124512.jpg'),
+(2, '/review/b631f9d2-35d1-4b5f-a97f-52f4f41326e3_20240706_124512.jpg'),
+(3, '/review/b631f9d2-35d1-4b5f-a97f-52f4f41326e3_20240706_124512.jpg'),
+(4, '/review/b631f9d2-35d1-4b5f-a97f-52f4f41326e3_20240706_124512.jpg'),
+(4, '/review/b631f9d2-35d1-4b5f-a97f-52f4f41326e3_20240706_124512.jpg'),
+(8, '/review/b631f9d2-35d1-4b5f-a97f-52f4f41326e3_20240706_124512.jpg'),
+(9, '/review/b631f9d2-35d1-4b5f-a97f-52f4f41326e3_20240706_124512.jpg'),
+(11, '/review/b631f9d2-35d1-4b5f-a97f-52f4f41326e3_20240706_124512.jpg'),
+(13, '/review/b631f9d2-35d1-4b5f-a97f-52f4f41326e3_20240706_124512.jpg');
 
 INSERT INTO review_keyword_mapping_tb (review_idx, keyword_idx) VALUES
 (1, 1),
