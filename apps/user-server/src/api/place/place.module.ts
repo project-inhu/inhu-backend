087f3b4ed0e94bd7f63b5e35219b8e8a8cc9887d
jochongs <joch2712@naver.com>
2025-07-18 16:09:44 +0900

    chore: Delete unncessary import in place domain

--- apps/user-server/src/api/place/place.module.ts
+++ apps/user-server/src/api/place/place.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { PlaceController } from './place.controller';
 import { PlaceService } from './place.service';
-import { PlaceRepository } from './place.repository';
 import { AuthModule } from '@user/auth/auth.module';
 import { PlaceCoreModule } from '@app/core';
 
