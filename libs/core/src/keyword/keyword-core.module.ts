07b3f0ebc6d7ab7fbe6aa470089e17b96c203736
jochongs <joch2712@naver.com>
2025-07-18 14:30:23 +0900

    chore: Connect keyword core repository and service to keyword module

--- libs/core/src/keyword/keyword-core.module.ts
+++ libs/core/src/keyword/keyword-core.module.ts
@@ -1,8 +1,10 @@
+import { KeywordCoreRepository } from '@app/core/keyword/keyword-core.repository';
+import { KeywordCoreService } from '@app/core/keyword/keyword-core.service';
 import { Module } from '@nestjs/common';
 
 @Module({
   imports: [],
-  providers: [],
-  exports: [],
+  providers: [KeywordCoreService, KeywordCoreRepository],
+  exports: [KeywordCoreService],
 })
 export class KeywordCoreModule {}
