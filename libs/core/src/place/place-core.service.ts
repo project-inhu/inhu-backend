b1f791756d6d77da49522add7d94856836f81ccc
jochongs <joch2712@naver.com>
2025-07-18 14:01:22 +0900

    feat: Implement getPlaceAll method in PlaceCoreService

--- libs/core/src/place/place-core.service.ts
+++ libs/core/src/place/place-core.service.ts
@@ -1,3 +1,4 @@
+import { GetPlaceOverviewInput } from '@app/core/place/inputs/get-place-overview.input';
 import { PlaceOverviewModel } from '@app/core/place/model/place-overview.model';
 import { PlaceCoreRepository } from '@app/core/place/place-core.repository';
 import { Injectable } from '@nestjs/common';
@@ -9,5 +10,13 @@ export class PlaceCoreService {
   public async getPlaceByIdx(idx: number): Promise<PlaceOverviewModel | null> {
     const place = await this.placeCoreRepository.selectPlaceByIdx(idx);
     return place && PlaceOverviewModel.fromPrisma(place);
+  }
+
+  public async getPlaceAll(
+    input: GetPlaceOverviewInput,
+  ): Promise<PlaceOverviewModel[]> {
+    return (await this.placeCoreRepository.selectPlaceAll(input)).map(
+      PlaceOverviewModel.fromPrisma,
+    );
   }
 }
