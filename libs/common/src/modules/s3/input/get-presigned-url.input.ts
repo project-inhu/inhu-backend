09db043bca615df7787aaa27e3be172723eb983b
hj-lab <heeju03180@naver.com>
2025-07-18 17:11:31 +0900

    refactor: Migrate S3 module to shared library

---
+++ libs/common/src/modules/s3/input/get-presigned-url.input.ts
@@ -0,0 +1,11 @@
+import { S3Folder } from '../constants/s3-folder.constants';
+
+/**
+ * Presigned URL 생성을 위한 입력 값
+ *
+ * @author 조희주
+ */
+export class GetPresignedUrlInput {
+  folder: S3Folder;
+  filename: string;
+}
