6d483b79fb1fbc285434f7f30abdb6df542c3e4d
jochongs <joch2712@naver.com>
2025-07-18 14:27:03 +0900

    feat: Implement getKeywordByIdx method

--- libs/core/src/keyword/keyword-core.service.ts
+++ libs/core/src/keyword/keyword-core.service.ts
@@ -1,6 +1,14 @@
+import { KeywordCoreRepository } from './keyword-core.repository';
 import { Injectable } from '@nestjs/common';
+import { KeywordModel } from './model/keyword.model';
 
 @Injectable()
 export class KeywordCoreService {
-  constructor() {}
+  constructor(private readonly keywordCoreRepository: KeywordCoreRepository) {}
+
+  public async getKeywordByIdx(idx: number): Promise<KeywordModel | null> {
+    const keyword = await this.keywordCoreRepository.selectKeywordByIdx(idx);
+
+    return keyword && KeywordModel.fromPrisma(keyword);
+  }
 }
