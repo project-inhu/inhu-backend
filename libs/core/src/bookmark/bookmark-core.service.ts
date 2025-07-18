5a7df02b20d927691adc1720fb89314c065ba77d
jochongs <joch2712@naver.com>
2025-07-18 15:01:46 +0900

    feat: Implement getBookmarkByIdx method in BookmarkCoreService

--- libs/core/src/bookmark/bookmark-core.service.ts
+++ libs/core/src/bookmark/bookmark-core.service.ts
@@ -1,7 +1,20 @@
+import { BookmarkModel } from '@app/core/bookmark/model/bookmark.model';
 import { BookmarkCoreRepository } from './bookmark-core.repository';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class BookmarkCoreService {
   constructor(private readonly bookmarkCoreReposit: BookmarkCoreRepository) {}
+
+  public async getBookmarkByIdx(
+    userIdx: number,
+    placeIdx: number,
+  ): Promise<BookmarkModel | null> {
+    const bookmark = await this.bookmarkCoreReposit.selectBookmarkByIdx(
+      userIdx,
+      placeIdx,
+    );
+
+    return bookmark ? BookmarkModel.fromPrisma(bookmark) : null;
+  }
 }
