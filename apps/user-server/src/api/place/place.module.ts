f47967f4aefb2d8fc2934c9e6374d7e5dfe8b270
jochongs <joch2712@naver.com>
2025-07-18 14:09:11 +0900

    chore: Delete unncessary import

--- apps/user-server/src/api/place/place.module.ts
+++ apps/user-server/src/api/place/place.module.ts
@@ -4,12 +4,10 @@ import { PlaceService } from './place.service';
 import { PlaceRepository } from './place.repository';
 import { KeywordModule } from '../keyword/keyword.module';
 import { AuthModule } from '@user/auth/auth.module';
-import { DateUtilModule } from '@user/common/module/date-util/date-util.module';
 
 @Module({
-  imports: [KeywordModule, AuthModule, DateUtilModule],
+  imports: [KeywordModule, AuthModule],
   controllers: [PlaceController],
   providers: [PlaceService, PlaceRepository],
-  exports: [PlaceService, PlaceRepository],
 })
 export class PlaceModule {}
