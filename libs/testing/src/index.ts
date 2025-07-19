bd7e058077aed918eb23befa45fe751a63b05597
jochongs <joch2712@naver.com>
2025-07-19 17:28:11 +0900

    chore: Add exporting picked place seed files to index.tsx

--- libs/testing/src/index.ts
+++ libs/testing/src/index.ts
@@ -10,3 +10,8 @@ export * from './seed/user/type/user-seed.output';
 export * from './seed/place/place.seed';
 export * from './seed/place/type/place-seed.input';
 export * from './seed/place/type/place-seed.output';
+
+// Picked Place Seed
+export * from './seed/picked-place/picked-place.seed';
+export * from './seed/picked-place/type/picked-place-seed.input';
+export * from './seed/picked-place/type/picked-place-seed.output';
