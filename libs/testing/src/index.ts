0a2daf33f404253bca9278187ac4cfa42336739c
jochongs <joch2712@naver.com>
2025-07-19 17:17:58 +0900

    chore: Add export code line in testing library

--- libs/testing/src/index.ts
+++ libs/testing/src/index.ts
@@ -1,4 +1,12 @@
 export * from './interface/test-helper.interface';
 export * from './interface/seed-helper.interface';
 
+// User Seed
 export * from './seed/user/user.seed';
+export * from './seed/user/type/user-seed.input';
+export * from './seed/user/type/user-seed.output';
+
+// Place Seed
+export * from './seed/place/place.seed';
+export * from './seed/place/type/place-seed.input';
+export * from './seed/place/type/place-seed.output';
