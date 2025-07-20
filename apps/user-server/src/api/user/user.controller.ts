451741d4d09c685c43561e7f5c351613e92b0ff6
jochongs <joch2712@naver.com>
2025-07-20 18:36:19 +0900

    chore: fix

--- apps/user-server/src/api/user/user.controller.ts
+++ apps/user-server/src/api/user/user.controller.ts
@@ -24,22 +24,22 @@ export class UserController {
     return await this.userService.getUserByIdx(user.idx);
   }
 
-  /**
-   * 프로필 이미지 업로드를 위한 Presigned URL 발급
-   * 프론트엔드에서 이 URL을 받아 S3에 파일을 직접 업로드
-   *
-   * @author 조희주
-   */
-  @UseGuards(AuthGuard)
-  @Get('/profile-image/presigned-url')
-  async getProfileImagePresignedUrl(
-    @Query('fileName') filename: string,
-  ): Promise<GetPresignedUrlResponseDto> {
-    return await this.s3Service.getPresignedUrl({
-      folder: S3_FOLDER.PROFILE,
-      filename,
-    });
-  }
+  // /**
+  //  * 프로필 이미지 업로드를 위한 Presigned URL 발급
+  //  * 프론트엔드에서 이 URL을 받아 S3에 파일을 직접 업로드
+  //  *
+  //  * @author 조희주
+  //  */
+  // @UseGuards(AuthGuard)
+  // @Get('/profile-image/presigned-url')
+  // async getProfileImagePresignedUrl(
+  //   @Query('fileName') filename: string,
+  // ): Promise<GetPresignedUrlResponseDto> {
+  //   return await this.s3Service.getPresignedUrl({
+  //     folder: S3_FOLDER.PROFILE,
+  //     filename,
+  //   });
+  // }
 
   // /**
   //  * 내 정보 수정
