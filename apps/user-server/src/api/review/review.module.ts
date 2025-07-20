e0e1de8b01a86b2a1849e1daec9d2f2b0cb7a6a0
jochongs <joch2712@naver.com>
2025-07-20 19:41:57 +0900

    fix: Resolve dependencies in ReviewModule

--- apps/user-server/src/api/review/review.module.ts
+++ apps/user-server/src/api/review/review.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ReviewController } from './review.controller';
 import { ReviewService } from './review.service';
-import { PlaceModule } from '../place/place.module';
-import { KeywordModule } from '../keyword/keyword.module';
-import { UserModule } from '../user/user.module';
 import { ReviewCoreModule } from '@libs/core/review/review-core.module';
+import { PlaceCoreModule } from '@libs/core';
 
 @Module({
-  imports: [ReviewCoreModule],
+  imports: [ReviewCoreModule, PlaceCoreModule],
   controllers: [ReviewController],
   providers: [ReviewService],
   exports: [ReviewService],
