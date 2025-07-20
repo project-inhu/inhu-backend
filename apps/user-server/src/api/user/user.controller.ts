a8caa6866dd3eba20b1b632c320d05a5aaa783ca
jochongs <joch2712@naver.com>
2025-07-20 18:35:36 +0900

    chore: Clean up import code line in UserController

--- apps/user-server/src/api/user/user.controller.ts
+++ apps/user-server/src/api/user/user.controller.ts
@@ -1,10 +1,8 @@
-import { Body, Controller, Get, Put, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
-import { AuthGuard } from '@user/auth/common/guards/auth.guard';
 import { User } from '@user/common/decorator/user.decorator';
 import { GetUserResponseDto } from './dto/response/get-user-response.dto';
-import { S3_FOLDER, S3Service } from '@libs/common';
-import { GetPresignedUrlResponseDto } from './dto/response/get-presigned-url-response.dto';
+import { S3Service } from '@libs/common';
 import { LoginUser } from '@user/common/types/LoginUser';
 import { LoginAuth } from '@user/common/decorator/login-auth.decorator';
 
@@ -26,22 +24,22 @@ export class UserController {
     return await this.userService.getUserByIdx(user.idx);
   }
 
-  // /**
-  //  * 프로필 이미지 업로드를 위한 Presigned URL 발급
-  //  * 프론트엔드에서 이 URL을 받아 S3에 파일을 직접 업로드
-  //  *
-  //  * @author 조희주
-  //  */
-  // @UseGuards(AuthGuard)
-  // @Get('/profile-image/presigned-url')
-  // async getProfileImagePresignedUrl(
-  //   @Query('fileName') filename: string,
-  // ): Promise<GetPresignedUrlResponseDto> {
-  //   return await this.s3Service.getPresignedUrl({
-  //     folder: S3_FOLDER.PROFILE,
-  //     filename,
-  //   });
-  // }
+  /**
+   * 프로필 이미지 업로드를 위한 Presigned URL 발급
+   * 프론트엔드에서 이 URL을 받아 S3에 파일을 직접 업로드
+   *
+   * @author 조희주
+   */
+  @UseGuards(AuthGuard)
+  @Get('/profile-image/presigned-url')
+  async getProfileImagePresignedUrl(
+    @Query('fileName') filename: string,
+  ): Promise<GetPresignedUrlResponseDto> {
+    return await this.s3Service.getPresignedUrl({
+      folder: S3_FOLDER.PROFILE,
+      filename,
+    });
+  }
 
   // /**
   //  * 내 정보 수정
