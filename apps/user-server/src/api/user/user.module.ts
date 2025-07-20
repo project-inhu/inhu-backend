a8f8c5774cef02bbd4ebc2f68ef88c5ffc01e717
jochongs <joch2712@naver.com>
2025-07-20 18:24:11 +0900

    chore: Update UserController

--- apps/user-server/src/api/user/user.module.ts
+++ apps/user-server/src/api/user/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { UserCoreModule } from '@libs/core';
+import { S3Module } from '@libs/common';
 
 @Module({
-  imports: [],
+  imports: [UserCoreModule, S3Module],
   controllers: [UserController],
   providers: [UserService],
 })
