06014b8a3772cafed23d117753e18a38f579cf4e
JEONG-YEON-KANG <kjykjy3314@naver.com>
2025-07-20 18:09:30 +0900

    fix: Replace repo function calls with service funcion calls

--- libs/core/src/review/review-core.module.ts
+++ libs/core/src/review/review-core.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ReviewCoreService } from './review-core.service';
 import { ReviewCoreRepository } from './review-core.repository';
-import { PlaceCoreRepository } from '@app/core/place/place-core.repository';
+import { PlaceCoreModule } from '../place/place-core.module';
 
 @Module({
-  providers: [ReviewCoreService, ReviewCoreRepository, PlaceCoreRepository],
+  imports: [PlaceCoreModule],
+  providers: [ReviewCoreService, ReviewCoreRepository],
   exports: [ReviewCoreService],
 })
 export class ReviewCoreModule {}
