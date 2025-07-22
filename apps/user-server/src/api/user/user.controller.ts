626d59eac366f36023116ddf2d1f3f681e695842
hj-lab <heeju03180@naver.com>
2025-07-22 14:39:50 +0900

    refactor: Change user update API from PUT to PATCH

--- apps/user-server/src/api/user/user.controller.ts
+++ apps/user-server/src/api/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Patch } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from '@user/common/decorator/user.decorator';
 import { LoginUser } from '@user/common/types/LoginUser';
@@ -8,7 +8,7 @@ import { UserEntity } from './entity/user.entity';
 
 @Controller('user')
 export class UserController {
-  constructor(private userService: UserService) {}
+  constructor(private readonly userService: UserService) {}
 
   /**
    * 내 정보 조회
@@ -26,7 +26,7 @@ export class UserController {
    *
    * @author 조희주
    */
-  @Put()
+  @Patch()
   @LoginAuth()
   async updateUserByIdx(
     @User() user: LoginUser,
