78ce56827d62f454d0fb7391705a8c28e41e973c
hj-lab <heeju03180@naver.com>
2025-07-23 00:37:12 +0900

    refactor: Add LoginAuth in getProfileImagePresignedUrl

--- apps/user-server/src/api/s3-upload/s3-upload.controller.ts
+++ apps/user-server/src/api/s3-upload/s3-upload.controller.ts
@@ -2,11 +2,13 @@ import { Controller, Get, Query } from '@nestjs/common';
 import { S3UploadService } from './s3-upload.service';
 import { PresignedUrlEntity } from './entity/pregiend-url.entity';
 import { S3_FOLDER } from '@libs/common';
+import { LoginAuth } from '@user/common/decorator/login-auth.decorator';
 @Controller('s3-upload')
 export class S3UploadController {
   constructor(private readonly s3UploadService: S3UploadService) {}
 
   @Get('/profile-image/presigned-url')
+  @LoginAuth()
   async getProfileImagePresignedUrl(
     @Query('filename') filename: string,
   ): Promise<PresignedUrlEntity> {
