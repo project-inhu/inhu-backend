cad382699ac64ebe3a549c0a388597dc2ffac9ef
jochongs <joch2712@naver.com>
2025-07-22 15:02:06 +0900

    feat: Implement GET /palce/:placeIdx/menu API

---
+++ apps/user-server/src/api/menu/menu.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { GetMenuByPlaceIdxDto } from '@user/api/menu/dto/request/get-menu-by-place-idx.dto';
+import { GetMenuByPlaceIdxResponseDto } from '@user/api/menu/dto/response/get-menu-by-place-idx-response.dto';
+import { MenuService } from '@user/api/menu/menu.service';
+
+@Controller()
+@ApiTags('menu')
+export class MenuController {
+  constructor(private readonly menuService: MenuService) {}
+
+  /**
+   * 메뉴 목록 보기
+   */
+  @Get('/place/:placeIdx/menu')
+  public async getMenuList(
+    @Param('placeIdx', ParseIntPipe) placeIdx: number,
+    @Query() dto: GetMenuByPlaceIdxDto,
+  ): Promise<GetMenuByPlaceIdxResponseDto> {
+    return await this.menuService.getMenuByPlaceIdx(placeIdx, dto);
+  }
+}
