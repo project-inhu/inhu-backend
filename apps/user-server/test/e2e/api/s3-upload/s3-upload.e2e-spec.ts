edfb7dc5673157a5ebcc4944e969d87b711c0d41
hj-lab <heeju03180@naver.com>
2025-07-23 01:25:41 +0900

    test: Add 400 test case for S3Upload

--- apps/user-server/test/e2e/api/s3-upload/s3-upload.e2e-spec.ts
+++ apps/user-server/test/e2e/api/s3-upload/s3-upload.e2e-spec.ts
@@ -1,3 +1,4 @@
+import { S3_FOLDER } from '@libs/common';
 import { PresignedUrlEntity } from '@user/api/s3-upload/entity/pregiend-url.entity';
 import { AppModule } from '@user/app.module';
 import { TestHelper } from 'apps/user-server/test/e2e/setup/test.helper';
@@ -35,6 +36,48 @@ describe('s3-upload E2E test', () => {
 
     it('401 - no accessToken', async () => {
       await testHelper.test().get('/user').expect(401);
+    });
+
+    it('400 - folder and filename are not provided', async () => {
+      const loginUser = testHelper.loginUsers.user1;
+
+      await testHelper
+        .test()
+        .get('/s3-upload/profile-image/presigned-url')
+        .set('Authorization', `Bearer ${loginUser.app.accessToken}`)
+        .expect(400);
+    });
+
+    it('400 - filename is not provided', async () => {
+      const loginUser = testHelper.loginUsers.user1;
+
+      await testHelper
+        .test()
+        .get('/s3-upload/profile-image/presigned-url')
+        .set('Authorization', `Bearer ${loginUser.app.accessToken}`)
+        .query({ folder: S3_FOLDER.PROFILE })
+        .expect(400);
+    });
+
+    it('400 - folder is not provided', async () => {
+      const loginUser = testHelper.loginUsers.user1;
+
+      await testHelper
+        .test()
+        .get('/s3-upload/profile-image/presigned-url')
+        .set('Authorization', `Bearer ${loginUser.app.accessToken}`)
+        .query({ filename: 'test-image.jpg' })
+        .expect(400);
+    });
+
+    it('400 - folder is not a valid S3Folder type', async () => {
+      const loginUser = testHelper.loginUsers.user1;
+      await testHelper
+        .test()
+        .get('/s3-upload/profile-image/presigned-url')
+        .set('Authorization', `Bearer ${loginUser.app.accessToken}`)
+        .query({ folder: 'invalid-folder', filename: 'test-image.jpg' })
+        .expect(400);
     });
   });
 });
