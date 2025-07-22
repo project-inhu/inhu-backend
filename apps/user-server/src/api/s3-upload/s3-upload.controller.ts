7a5063e36f84a8feee01d4c12aa0515ce5b4c293
hj-lab <heeju03180@naver.com>
2025-07-23 02:45:13 +0900

    refactor: Modify API endpoint to use POST with request body

--- apps/user-server/src/api/s3-upload/s3-upload.controller.ts
+++ apps/user-server/src/api/s3-upload/s3-upload.controller.ts
@@ -1,20 +1,21 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { S3UploadService } from './s3-upload.service';
 import { PresignedUrlEntity } from './entity/pregiend-url.entity';
 import { S3_FOLDER } from '@libs/common';
 import { LoginAuth } from '@user/common/decorator/login-auth.decorator';
+import { GetPresignedUrlDto } from './dto/request/get-presigned-url.dto';
 @Controller('s3-upload')
 export class S3UploadController {
   constructor(private readonly s3UploadService: S3UploadService) {}
 
-  @Get('/profile-image/presigned-url')
+  @Post('/profile-image/presigned-url')
   @LoginAuth()
   async getProfileImagePresignedUrl(
-    @Query('filename') filename: string,
+    @Body() getPresignedUrlDto: GetPresignedUrlDto,
   ): Promise<PresignedUrlEntity> {
     return this.s3UploadService.getProfileImagePresignedUrl({
       folder: S3_FOLDER.PROFILE,
-      filename: filename,
+      filename: getPresignedUrlDto.filename,
     });
   }
 }
