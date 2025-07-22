4d1477db29db5831cee4502b95da73cbd84f3639
hj-lab <heeju03180@naver.com>
2025-07-23 00:15:15 +0900

    feat: Add getProfileImagePresignedUrl in Controller

--- apps/user-server/src/api/s3-upload/s3-upload.controller.ts
+++ apps/user-server/src/api/s3-upload/s3-upload.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Get, Query } from '@nestjs/common';
+import { S3UploadService } from './s3-upload.service';
+import { PresignedUrlEntity } from './entity/pregiend-url.entity';
+import { S3_FOLDER } from '@libs/common';
 @Controller('s3-upload')
-export class S3UploadController {}
+export class S3UploadController {
+  constructor(private readonly s3UploadService: S3UploadService) {}
+
+  @Get('/profile-image/presigned-url')
+  async getProfileImagePresignedUrl(
+    @Query('filename') filename: string,
+  ): Promise<PresignedUrlEntity> {
+    return this.s3UploadService.getProfileImagePresignedUrl({
+      folder: S3_FOLDER.PROFILE,
+      filename: filename,
+    });
+  }
+}
