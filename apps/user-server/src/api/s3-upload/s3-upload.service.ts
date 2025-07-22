4b63d54b2c86bc4ff3f193ea619a21032600ef46
hj-lab <heeju03180@naver.com>
2025-07-23 00:15:30 +0900

    feat: Add getProfileImagePresignedUrl in Service

--- apps/user-server/src/api/s3-upload/s3-upload.service.ts
+++ apps/user-server/src/api/s3-upload/s3-upload.service.ts
@@ -1,4 +1,14 @@
+import { PresignedUrlModel, S3Service } from '@libs/common';
 import { Injectable } from '@nestjs/common';
+import { GetPresignedUrlDto } from './dto/request/get-presigned-url.dto';
 
 @Injectable()
-export class S3UploadService {}
+export class S3UploadService {
+  constructor(private readonly s3Service: S3Service) {}
+
+  public async getProfileImagePresignedUrl(
+    getPresignedUrlDto: GetPresignedUrlDto,
+  ): Promise<PresignedUrlModel> {
+    return this.s3Service.getPresignedUrl(getPresignedUrlDto);
+  }
+}
