dc77f8588f4967801ba5b8294db532b2d27c22c5
hj-lab <heeju03180@naver.com>
2025-07-23 01:14:51 +0900

    test: Add 200 test case for S3Upload

--- apps/user-server/test/e2e/api/s3-upload/s3-upload.e2e-spec.ts
+++ apps/user-server/test/e2e/api/s3-upload/s3-upload.e2e-spec.ts
@@ -1,3 +1,4 @@
+import { PresignedUrlEntity } from '@user/api/s3-upload/entity/pregiend-url.entity';
 import { AppModule } from '@user/app.module';
 import { TestHelper } from 'apps/user-server/test/e2e/setup/test.helper';
 
@@ -10,5 +11,26 @@ describe('s3-upload E2E test', () => {
 
   afterEach(async () => {
     await testHelper.destroy();
+  });
+
+  describe('GET /s3-upload/profile-image/presigned-url', () => {
+    it('200 - successfully retrieves presigned URL for profile image upload', async () => {
+      const filename = 'test-image.jpg';
+      const response = await testHelper
+        .test()
+        .get('/s3-upload/profile-image/presigned-url')
+        .query({ filename })
+        .set(
+          'Authorization',
+          `Bearer ${testHelper.loginUsers.user1.app.accessToken}`,
+        )
+        .expect(200);
+
+      const responseBody: PresignedUrlEntity = response.body;
+
+      expect(responseBody).toBeDefined();
+      expect(responseBody).toHaveProperty('presignedUrl');
+      expect(responseBody).toHaveProperty('key');
+    });
   });
 });
