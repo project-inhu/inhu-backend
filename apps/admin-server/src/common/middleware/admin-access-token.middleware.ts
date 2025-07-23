8148fff162fcac2cb9c817d27a47dbcc9f7487b6
SuinLee21 <suin7473@naver.com>
2025-07-23 19:17:26 +0900

    feat: Implement admin access token middleware

---
+++ apps/admin-server/src/common/middleware/admin-access-token.middleware.ts
@@ -0,0 +1,37 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { LoginTokenService } from '../modules/login-token/login-token.service';
+import { NextFunction, Request } from 'express';
+
+@Injectable()
+export class AdminAccessTokenMiddleware implements NestMiddleware {
+  constructor(private readonly loginTokenService: LoginTokenService) {}
+
+  async use(req: Request, res: Response, next: NextFunction) {
+    const adminToken = this.extractAccessToken(req);
+
+    if (!adminToken) {
+      return next();
+    }
+
+    const payload = await this.loginTokenService.verifyAdminToken(adminToken);
+    (req as any).admin = payload;
+
+    next();
+  }
+
+  private extractAccessToken(req: Request): string | null {
+    const token = req.headers.authorization;
+
+    if (!token) {
+      return null;
+    }
+
+    const [type, adminToken] = token.split(' ');
+
+    if (type !== 'Bearer' || !adminToken) {
+      return null;
+    }
+
+    return adminToken;
+  }
+}
