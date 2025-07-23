95d52ace625fe1a2b1795f5741369fe02525285e
SuinLee21 <suin7473@naver.com>
2025-07-23 19:19:22 +0900

    fix: Resolve dependency injection error caused by incorrect service import

--- apps/admin-server/src/admin-server.module.ts
+++ apps/admin-server/src/admin-server.module.ts
@@ -1,8 +1,37 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { AuthModule } from './api/auth/auth.module';
+import { AdminAccessTokenMiddleware } from './common/middleware/access-token.middleware';
+import { PrismaModule } from '@libs/common/modules/prisma/prisma.module';
+import { ClsModule } from 'nestjs-cls';
+import { ClsPluginTransactional } from '@nestjs-cls/transactional';
+import { TransactionalAdapterPrisma } from '@nestjs-cls/transactional-adapter-prisma';
+import { PrismaService } from '@libs/common/modules/prisma/prisma.service';
+import { ConfigModule } from '@nestjs/config';
+import { LoginTokenModule } from './common/modules/login-token/login-token.module';
 
 @Module({
-  imports: [],
+  imports: [
+    PrismaModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    LoginTokenModule,
+    ClsModule.forRoot({
+      plugins: [
+        new ClsPluginTransactional({
+          imports: [PrismaModule],
+          adapter: new TransactionalAdapterPrisma({
+            prismaInjectionToken: PrismaService,
+            sqlFlavor: 'postgresql',
+          }),
+        }),
+      ],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
-export class AdminServerModule {}
+export class AdminServerModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    return consumer.apply(AdminAccessTokenMiddleware).forRoutes('/');
+  }
+}
