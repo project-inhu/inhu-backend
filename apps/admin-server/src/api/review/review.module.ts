152509b63aa8967cbd8d685352711fda6bdc4d59
JEONG-YEON-KANG <kjykjy3314@naver.com>
2025-07-25 14:48:43 +0900

    fix: Change the route to admin-server

--- apps/admin-server/src/api/review/review.module.ts
+++ apps/admin-server/src/api/review/review.module.ts
@@ -1,6 +1,6 @@
 import { ReviewCoreModule } from '@libs/core/review/review-core.module';
 import { Module } from '@nestjs/common';
-import { ReviewController } from '@user/api/review/review.controller';
+import { ReviewController } from './review.controller';
 import { ReviewService } from './review.service';
 
 @Module({
