7661a7eda767bffdb2626915b30dd9b021f271ba
JEONG-YEON-KANG <kjykjy3314@naver.com>
2025-07-25 16:02:17 +0900

    feat: Add delete review API

--- apps/admin-server/src/api/review/review.service.ts
+++ apps/admin-server/src/api/review/review.service.ts
@@ -1,31 +1,11 @@
 import { ReviewCoreService } from '@libs/core/review/review-core.service';
 import { Injectable } from '@nestjs/common';
-import { GetAllReviewDto } from './dto/request/get-all-review.dto';
-import { ReviewEntity } from './entity/review.entity';
 
 @Injectable()
 export class ReviewService {
   constructor(private readonly reviewCoreService: ReviewCoreService) {}
 
-  public async getAllReview(dto: GetAllReviewDto): Promise<{
-    reviewList: ReviewEntity[];
-    hasNext: boolean;
-  }> {
-    const pageSize = 10;
-    const take = pageSize + 1;
-    const skip = (dto.page - 1) * pageSize;
-
-    const reviewModelList = await this.reviewCoreService.getAllReview({
-      take,
-      skip,
-      ...dto,
-    });
-
-    return {
-      reviewList: reviewModelList
-        .slice(0, pageSize)
-        .map((review) => ReviewEntity.fromModel(review)),
-      hasNext: reviewModelList.length > pageSize,
-    };
+  async deleteReviewByIdx(reviewIdx: number): Promise<void> {
+    await this.reviewCoreService.deleteReviewByIdx(reviewIdx);
   }
 }
