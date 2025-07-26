1470cb8118664b9b757cc39c5a5a0176e9b19b53
SuinLee21 <suin7473@naver.com>
2025-07-26 17:39:53 +0900

    chore: Add ApiBearerAuth decorator

--- apps/user-server/src/common/decorator/login-auth.decorator.ts
+++ apps/user-server/src/common/decorator/login-auth.decorator.ts
@@ -3,11 +3,13 @@ import { TokenIssuedBy } from '@user/common/module/login-token/constants/token-i
 import { Exception } from '@libs/common';
 import { AppLoginAuthGuard } from '@user/api/auth/app-login-auth.guard';
 import { LoginAuthGuard } from '@user/api/auth/login-auth.guard';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 export const LoginAuth = (issuedBy: TokenIssuedBy = TokenIssuedBy.WEB) => {
   if (issuedBy === TokenIssuedBy.APP) {
     return applyDecorators(
       UseGuards(AppLoginAuthGuard),
+      ApiBearerAuth(),
       Exception(401, 'token not provided or expired'),
       Exception(403, 'Access denied'),
     );
@@ -15,6 +17,7 @@ export const LoginAuth = (issuedBy: TokenIssuedBy = TokenIssuedBy.WEB) => {
 
   return applyDecorators(
     UseGuards(LoginAuthGuard),
+    ApiBearerAuth(),
     Exception(401, 'token not provided or expired'),
   );
 };
