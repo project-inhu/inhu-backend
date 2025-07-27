b57527949cd29fb5815c5aa1029fac35ea569d08
hj-lab <heeju03180@naver.com>
2025-07-27 16:09:21 +0900

    refactor: Rename DTO in s3-upload

--- apps/user-server/src/api/s3-upload/dto/request/create-profile-image-presigned-url.dto.ts
+++ apps/user-server/src/api/s3-upload/dto/request/create-profile-image-presigned-url.dto.ts
@@ -1,20 +1,20 @@
 import { S3_FOLDER, S3Folder } from '@libs/common';
-import { IsIn, IsNotEmpty, IsString } from 'class-validator';
+import { ArrayMaxSize, IsIn, IsNotEmpty, IsString } from 'class-validator';
 
 /**
  * Presigned URL 생성을 위한 입력 값
  *
  * @author 조희주
  */
-export class CreatePresignedUrlDto {
-  /**
-   * S3에 업로드할 파일의 폴더 경로
-   *
-   * @example "profile"
-   */
-  @IsNotEmpty()
-  @IsIn(Object.values(S3_FOLDER))
-  folder: S3Folder;
+export class CreateProfileImagePresignedUrlDto {
+  // /**
+  //  * S3에 업로드할 파일의 폴더 경로
+  //  *
+  //  * @example "profile"
+  //  */
+  // @IsNotEmpty()
+  // @IsIn(Object.values(S3_FOLDER))
+  // folder: S3Folder;
 
   /**
    * 업로드할 파일의 원본 이름
