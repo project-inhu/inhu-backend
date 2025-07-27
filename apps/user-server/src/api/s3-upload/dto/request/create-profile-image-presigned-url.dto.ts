1013b44e7b041a77d2aeabc39c9de3029521e29d
hj-lab <heeju03180@naver.com>
2025-07-27 16:38:38 +0900

    refactor: Delete unused import and comment

--- apps/user-server/src/api/s3-upload/dto/request/create-profile-image-presigned-url.dto.ts
+++ apps/user-server/src/api/s3-upload/dto/request/create-profile-image-presigned-url.dto.ts
@@ -7,15 +7,6 @@ import { ArrayMaxSize, IsIn, IsNotEmpty, IsString } from 'class-validator';
  * @author 조희주
  */
 export class CreateProfileImagePresignedUrlDto {
-  // /**
-  //  * S3에 업로드할 파일의 폴더 경로
-  //  *
-  //  * @example "profile"
-  //  */
-  // @IsNotEmpty()
-  // @IsIn(Object.values(S3_FOLDER))
-  // folder: S3Folder;
-
   /**
    * 업로드할 파일의 원본 이름
    * 프론트엔드에서 사용자가 선택한 파일의 .name 속성 값
