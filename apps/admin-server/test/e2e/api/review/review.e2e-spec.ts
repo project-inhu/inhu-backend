a5f04162f13257d6bebe6700f4ca59656fc0bb17
JEONG-YEON-KANG <kjykjy3314@naver.com>
2025-07-27 21:52:36 +0900

    feat: Add admin get reveiw all e2e test

--- apps/admin-server/test/e2e/api/review/review.e2e-spec.ts
+++ apps/admin-server/test/e2e/api/review/review.e2e-spec.ts
@@ -5,6 +5,7 @@ import {
   UserSeedHelper,
 } from '@libs/testing';
 import { AdminServerModule } from '@admin/admin-server.module';
+import { GetAllReviewResponseDto } from '@admin/api/review/dto/response/get-all-review.reponse.dto';
 
 describe('Admin Review E2E Tests', () => {
   const testHelper = TestHelper.create(AdminServerModule);
@@ -262,6 +263,415 @@ describe('Admin Review E2E Tests', () => {
         const after = afterMap.get(keywordIdx) ?? 0;
         expect(after).toBe(before);
       }
+    });
+  });
+
+  describe('GET /review/all', () => {
+    it('200 - review field check', async () => {
+      const loginUser = testHelper.loginAdmin.admin1;
+
+      const user = await userSeedHelper.seed({
+        nickname: 'testUser',
+        profileImagePath: '/profile/testUser.png',
+      });
+
+      const place = await placeSeedHelper.seed({
+        deletedAt: null,
+        activatedAt: new Date(),
+        roadAddress: {
+          name: '서울시 강남구 테헤란로 123',
+          detail: '테헤란로 123',
+        },
+      });
+
+      const reviewSeed = await reviewSeedHelper.seed({
+        placeIdx: place.idx,
+        userIdx: user.idx,
+        reviewImgList: [
+          '/review/3b54e245-4f4d-41a0-9c1b-2f5e2f473b38-20250719.jpg',
+          '/review/f994f6ad-c42f-4c29-86db-d391fe450b1f-20250719.jpg',
+        ],
+        keywordIdxList: [1, 2],
+      });
+
+      const response = await testHelper
+        .test()
+        .get('/review/all')
+        .query({
+          placeIdx: place.idx,
+          page: 1,
+        })
+        .set('Authorization', `Bearer ${loginUser.token}`)
+        .expect(200);
+
+      const body: GetAllReviewResponseDto = response.body;
+
+      const { reviewList } = body;
+
+      const resultReview = reviewList[0];
+
+      expect(resultReview.idx).toBe(reviewSeed.idx);
+      expect(resultReview.content).toBe(reviewSeed.content);
+      expect(resultReview.imagePathList.sort()).toStrictEqual(
+        (reviewSeed.reviewImgList || [])?.sort(),
+      );
+      expect(
+        resultReview.keywordList.map(({ idx }) => idx).sort(),
+      ).toStrictEqual((reviewSeed.keywordIdxList || [])?.sort());
+      expect(resultReview.place.idx).toBe(place.idx);
+      expect(resultReview.place.name).toBe(place.name);
+      expect(resultReview.place.roadAddress.name).toBe(place.roadAddress.name);
+      expect(resultReview.place.roadAddress.detail).toBe(
+        place.roadAddress.detail,
+      );
+      expect(resultReview.place.roadAddress.addressX).toBe(
+        place.roadAddress.addressX,
+      );
+      expect(resultReview.place.roadAddress.addressY).toBe(
+        place.roadAddress.addressY,
+      );
+      expect(resultReview.author.idx).toBe(user.idx);
+      expect(resultReview.author.nickname).toBe(user.nickname);
+      expect(resultReview.author.profileImagePath).toBe(user.profileImagePath);
+    });
+
+    it('200 - hasNext check', async () => {
+      const loginUser = testHelper.loginAdmin.admin1;
+
+      const user = await userSeedHelper.seed({
+        nickname: 'testUser',
+        profileImagePath: '/profile/testUser.png',
+      });
+
+      const place = await placeSeedHelper.seed({
+        deletedAt: null,
+        activatedAt: new Date(),
+      });
+
+      // placeIdx가 place.idx인 리뷰 10개 생성
+      await reviewSeedHelper.seedAll(
+        Array.from({ length: 10 }, () => ({
+          placeIdx: place.idx,
+          userIdx: user.idx,
+        })),
+      );
+
+      const response = await testHelper
+        .test()
+        .get('/review/all')
+        .query({
+          placeIdx: place.idx,
+          page: 1,
+        })
+        .set('Authorization', `Bearer ${loginUser.token}`)
+        .expect(200);
+
+      const body: GetAllReviewResponseDto = response.body;
+
+      expect(body.hasNext).toBe(false);
+
+      // 10개에서 하나 더 넣기
+      await reviewSeedHelper.seed({
+        placeIdx: place.idx,
+        userIdx: loginUser.idx,
+      });
+
+      const response2 = await testHelper
+        .test()
+        .get('/review/all')
+        .query({
+          placeIdx: place.idx,
+          page: 1,
+        })
+        .set('Authorization', `Bearer ${loginUser.token}`)
+        .expect(200);
+
+      expect(response2.body.hasNext).toBe(true);
+    });
+
+    it('200 - sort order asc check', async () => {
+      const loginUser = testHelper.loginAdmin.admin1;
+
+      const user = await userSeedHelper.seed({
+        nickname: 'testUser',
+        profileImagePath: '/profile/testUser.png',
+      });
+
+      const place = await placeSeedHelper.seed({
+        deletedAt: null,
+        activatedAt: new Date(),
+      });
+
+      const firstReview = await reviewSeedHelper.seed({
+        placeIdx: place.idx,
+        userIdx: user.idx,
+      });
+
+      // 시간 간격을 두어 생성 순서를 명확히 함
+      await new Promise((resolve) => setTimeout(resolve, 20));
+
+      const secondReview = await reviewSeedHelper.seed({
+        placeIdx: place.idx,
+        userIdx: user.idx,
+      });
+
+      const response = await testHelper
+        .test()
+        .get('/review/all')
+        .query({ placeIdx: place.idx, page: 1, orderBy: 'time', order: 'asc' })
+        .set('Authorization', `Bearer ${loginUser.token}`)
+        .expect(200);
+
+      const { reviewList }: GetAllReviewResponseDto = response.body;
+
+      expect(reviewList[0].idx).toBe(firstReview.idx);
+      expect(reviewList[1].idx).toBe(secondReview.idx);
+    });
+
+    it('200 - sort order desc check', async () => {
+      const loginUser = testHelper.loginAdmin.admin1;
+
+      const user = await userSeedHelper.seed({
+        nickname: 'testUser',
+        profileImagePath: '/profile/testUser.png',
+      });
+
+      const place = await placeSeedHelper.seed({
+        deletedAt: null,
+        activatedAt: new Date(),
+      });
+
+      const firstReview = await reviewSeedHelper.seed({
+        placeIdx: place.idx,
+        userIdx: user.idx,
+      });
+
+      // 시간 간격을 두어 생성 순서를 명확히 함
+      await new Promise((resolve) => setTimeout(resolve, 20));
+
+      const secondReview = await reviewSeedHelper.seed({
+        placeIdx: place.idx,
+        userIdx: user.idx,
+      });
+
+      const response = await testHelper
+        .test()
+        .get('/review/all')
+        .query({ placeIdx: place.idx, page: 1, orderBy: 'time', order: 'desc' })
+        .set('Authorization', `Bearer ${loginUser.token}`)
+        .expect(200);
+
+      const { reviewList }: GetAllReviewResponseDto = response.body;
+
+      expect(reviewList[0].idx).toBe(secondReview.idx);
+      expect(reviewList[1].idx).toBe(firstReview.idx);
+    });
+
+    it('200 - soft delete check', async () => {
+      const loginUser = testHelper.loginAdmin.admin1;
+
+      const user = await userSeedHelper.seed({
+        nickname: 'testUser',
+        profileImagePath: '/profile/testUser.png',
+      });
+
+      const place = await placeSeedHelper.seed({
+        deletedAt: null,
+        activatedAt: new Date(),
+      });
+
+      // 삭제된 리뷰 생성
+      await reviewSeedHelper.seed({
+        placeIdx: place.idx,
+        userIdx: user.idx,
+        deletedAt: new Date(),
+      });
+
+      const reviewToKeep = await reviewSeedHelper.seed({
+        placeIdx: place.idx,
+        userIdx: user.idx,
+      });
+
+      const response = await testHelper
+        .test()
+        .get('/review/all')
+        .query({ placeIdx: place.idx, page: 1 })
+        .set('Authorization', `Bearer ${loginUser.token}`)
+        .expect(200);
+
+      const { reviewList }: GetAllReviewResponseDto = response.body;
+
+      expect(reviewList[0].idx).toBe(reviewToKeep.idx);
+    });
+
+    it('200 - place idx filtering check', async () => {
+      const loginUser = testHelper.loginAdmin.admin1;
+
+      const user = await userSeedHelper.seed({
+        nickname: 'testUser',
+        profileImagePath: '/profile/testUser.png',
+      });
+
+      const [firstPlace, secondPlace] = await placeSeedHelper.seedAll([
+        { deletedAt: null, activatedAt: new Date() },
+        { deletedAt: null, activatedAt: new Date() },
+      ]);
+
+      const [
+        review1OfFirstPlace,
+        review2OfFirstPlace,
+        review3OfSecondPlace,
+        review4OfSecondPlace,
+      ] = await reviewSeedHelper.seedAll([
+        { placeIdx: firstPlace.idx, userIdx: user.idx },
+        { placeIdx: firstPlace.idx, userIdx: user.idx },
+        { placeIdx: secondPlace.idx, userIdx: user.idx },
+        { placeIdx: secondPlace.idx, userIdx: user.idx },
+      ]);
+
+      const firstPlaceResponse = await testHelper
+        .test()
+        .get('/review/all')
+        .query({
+          placeIdx: firstPlace.idx,
+          page: 1,
+        })
+        .set('Authorization', `Bearer ${loginUser.token}`)
+        .expect(200);
+
+      const reviewsOfFirstPlace = (
+        firstPlaceResponse.body as GetAllReviewResponseDto
+      ).reviewList;
+
+      expect(reviewsOfFirstPlace.map(({ idx }) => idx).sort()).toStrictEqual(
+        [review1OfFirstPlace.idx, review2OfFirstPlace.idx].sort(),
+      );
+
+      const secondPlaceResponse = await testHelper
+        .test()
+        .get('/review/all')
+        .query({
+          placeIdx: secondPlace.idx,
+          page: 1,
+        })
+        .set('Authorization', `Bearer ${loginUser.token}`)
+        .expect(200);
+
+      const reviewsOfSecondPlace = (
+        secondPlaceResponse.body as GetAllReviewResponseDto
+      ).reviewList;
+
+      expect(reviewsOfSecondPlace.map(({ idx }) => idx).sort()).toStrictEqual(
+        [review3OfSecondPlace.idx, review4OfSecondPlace.idx].sort(),
+      );
+    });
+
+    it('200 - user idx filtering check', async () => {
+      const loginUser = testHelper.loginAdmin.admin1;
+
+      const firstUser = await userSeedHelper.seed({
+        nickname: 'testUser',
+        profileImagePath: '/profile/testUser.png',
+      });
+
+      const secondUser = await userSeedHelper.seed({
+        nickname: 'testUser2',
+        profileImagePath: '/profile/testUser2.png',
+      });
+
+      const place = await placeSeedHelper.seed({
+        deletedAt: null,
+        activatedAt: new Date(),
+      });
+
+      const [
+        review1OfFirstUser,
+        review2OfFirstUser,
+        review3OfSecondUser,
+        review4OfSecondUser,
+      ] = await reviewSeedHelper.seedAll([
+        { placeIdx: place.idx, userIdx: firstUser.idx },
+        { placeIdx: place.idx, userIdx: firstUser.idx },
+        { placeIdx: place.idx, userIdx: secondUser.idx },
+        { placeIdx: place.idx, userIdx: secondUser.idx },
+      ]);
+
+      const firstUserResponse = await testHelper
+        .test()
+        .get('/review/all')
+        .query({
+          userIdx: firstUser.idx,
+          page: 1,
+        })
+        .set('Authorization', `Bearer ${loginUser.token}`)
+        .expect(200);
+
+      const reviewsOfFirstUser = (
+        firstUserResponse.body as GetAllReviewResponseDto
+      ).reviewList;
+
+      expect(reviewsOfFirstUser.map(({ idx }) => idx).sort()).toStrictEqual(
+        [review1OfFirstUser.idx, review2OfFirstUser.idx].sort(),
+      );
+
+      const secondUserResponse = await testHelper
+        .test()
+        .get('/review/all')
+        .query({
+          userIdx: secondUser.idx,
+          page: 1,
+        })
+        .set('Authorization', `Bearer ${loginUser.token}`)
+        .expect(200);
+
+      const reviewsOfSecondUser = (
+        secondUserResponse.body as GetAllReviewResponseDto
+      ).reviewList;
+
+      expect(reviewsOfSecondUser.map(({ idx }) => idx).sort()).toStrictEqual(
+        [review3OfSecondUser.idx, review4OfSecondUser.idx].sort(),
+      );
+    });
+
+    it('400 - invalid place idx', async () => {
+      const loginUser = testHelper.loginAdmin.admin1;
+
+      await testHelper
+        .test()
+        .get('/review/all')
+        .query({
+          placeIdx: 'invalid', // ! invalid place idx
+          page: 1,
+        })
+        .set('Authorization', `Bearer ${loginUser.token}`)
+        .expect(400);
+    });
+
+    it('400 - invalid user idx', async () => {
+      const loginUser = testHelper.loginAdmin.admin1;
+
+      await testHelper
+        .test()
+        .get('/review/all')
+        .query({
+          userIdx: 'invalid', // ! invalid place idx
+          page: 1,
+        })
+        .set('Authorization', `Bearer ${loginUser.token}`)
+        .expect(400);
+    });
+
+    it('403 - no placeIdx and no userIdx', async () => {
+      const loginUser = testHelper.loginAdmin.admin1;
+
+      await testHelper
+        .test()
+        .get('/review/all')
+        .query({
+          page: 1, // ! no placeIdx and no userIdx
+        })
+        .set('Authorization', `Bearer ${loginUser.token}`)
+        .expect(403);
     });
   });
 });
