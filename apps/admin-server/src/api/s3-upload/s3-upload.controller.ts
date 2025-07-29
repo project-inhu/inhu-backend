d7e7a986fc23776addef35d2b8afebe6c7c06cea
hj-lab <heeju03180@naver.com>
2025-07-29 16:20:42 +0900

    feat: Add createBannerImagePresignedUrl in controller

--- apps/admin-server/src/api/s3-upload/s3-upload.controller.ts
+++ apps/admin-server/src/api/s3-upload/s3-upload.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { S3UploadService } from '@admin/api/s3-upload/s3-upload.service';
+import { AdminAuth } from '@admin/common/decorator/admin-auth.decorator';
+import { CreateBannerImagePresignedUrlDto } from '@admin/api/s3-upload/dto/request/create-banner-image-presigned-url.dto';
+import { PresignedUrlEntity } from '@admin/api/s3-upload/entity/presigned-url.entity';
 
 @Controller('s3-upload')
 export class S3UploadController {
   constructor(private readonly s3UploadService: S3UploadService) {}
+
+  @Post('/banner-image/presigned-url')
+  @AdminAuth()
+  async createBannerImagePresignedUrl(
+    @Body() createBannerImagePresignedUrlDto: CreateBannerImagePresignedUrlDto,
+  ): Promise<PresignedUrlEntity> {
+    return this.s3UploadService.createBannerImagePresignedUrl(
+      createBannerImagePresignedUrlDto,
+    );
+  }
 }
