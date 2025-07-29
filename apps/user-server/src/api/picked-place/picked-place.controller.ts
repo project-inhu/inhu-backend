6ad3137c22523337647b43cfa04533707b2a87e9
JEONG-YEON-KANG <kjykjy3314@naver.com>
2025-07-29 17:32:35 +0900

    feat: Add get all picked place API

--- apps/user-server/src/api/picked-place/picked-place.controller.ts
+++ apps/user-server/src/api/picked-place/picked-place.controller.ts
@@ -1,25 +1,29 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { PickedPlaceService } from './picked-place.service';
 import { User } from '@user/common/decorator/user.decorator';
+import { Exception } from '@libs/common';
+import { GetAllPickedPlaceOverviewDto } from './dto/request/get-all-picked-place-overview.dto';
+import { LoginUser } from '@user/common/types/LoginUser';
+import { GetAllPickedPlaceOverviewResponseDto } from './dto/response/get-all-picked-place.response.dto';
 
 @Controller('picked-place')
 export class PickedPlaceController {
   constructor(private pickedPlaceService: PickedPlaceService) {}
 
-  // /**
-  //  * 선정된 장소 개요 (Picked Place) 모두 가져오기
-  //  *
-  //  * @author 강정연
-  //  */
-  // @Exception(400, 'Invalid page number')
-  // @Get('/all')
-  // async getAllPickedPlaceOverview(
-  //   @Query() getAllPickedPlaceOverviewDto: GetAllPickedPlaceOverviewDto,
-  //   @User('idx') userIdx?: number,
-  // ): Promise<GetAllPickedPlaceOverviewResponseDto> {
-  //   return await this.pickedPlaceService.getAllPickedPlaceOverview(
-  //     getAllPickedPlaceOverviewDto.page,
-  //     userIdx,
-  //   );
-  // }
+  /**
+   * 선정된 장소 개요 (Picked Place) 모두 가져오기
+   *
+   * @author 강정연
+   */
+  @Exception(400, 'Invalid page number')
+  @Get('/all')
+  async getAllPickedPlaceOverview(
+    @Query() dto: GetAllPickedPlaceOverviewDto,
+    @User() loginUser?: LoginUser,
+  ): Promise<GetAllPickedPlaceOverviewResponseDto> {
+    return await this.pickedPlaceService.getAllPickedPlaceOverview(
+      dto,
+      loginUser?.idx,
+    );
+  }
 }
--- apps/user-server/src/api/picked-place/picked-place.module.ts
+++ apps/user-server/src/api/picked-place/picked-place.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PickedPlaceController } from './picked-place.controller';
 import { PickedPlaceService } from './picked-place.service';
+import { BookmarkCoreModule, PickedPlaceCoreModule } from '@libs/core';
 
 @Module({
-  imports: [],
+  imports: [PickedPlaceCoreModule, BookmarkCoreModule],
   controllers: [PickedPlaceController],
   providers: [PickedPlaceService],
   exports: [PickedPlaceService],
