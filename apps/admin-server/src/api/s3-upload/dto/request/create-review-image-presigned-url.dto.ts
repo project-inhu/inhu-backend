44d214e51310d58644ed190704a96e463957c875
hj-lab <heeju03180@naver.com>
2025-07-29 23:01:26 +0900

    feat: Add CreateReviewImagePresignedUrlsDto

---
+++ apps/admin-server/src/api/s3-upload/dto/request/create-review-image-presigned-url.dto.ts
@@ -0,0 +1,30 @@
+import {
+  IMAGE_EXTENSION,
+  ImageExtension,
+} from '@libs/common/modules/s3/constants/image-extension.constants';
+import {
+  ArrayMaxSize,
+  ArrayNotEmpty,
+  IsArray,
+  IsIn,
+  IsString,
+} from 'class-validator';
+
+/**
+ * Presigned URL 여러개 생성을 위한 입력 값
+ *
+ * @author 조희주
+ */
+export class CreateReviewImagePresignedUrlsDto {
+  /**
+   * 업로드할 파일의 확장자명
+   *
+   * @example "jpg"
+   */
+  @IsArray()
+  @ArrayNotEmpty()
+  @IsString({ each: true })
+  @IsIn(Object.values(IMAGE_EXTENSION), { each: true })
+  @ArrayMaxSize(5)
+  extensions: ImageExtension[];
+}
