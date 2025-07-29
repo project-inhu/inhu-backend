e2bc0b8c93fc7eb6b2d9a3dd87fb49064fe92743
JEONG-YEON-KANG <kjykjy3314@naver.com>
2025-07-29 16:38:31 +0900

    feat: Add get all picked place API

--- apps/user-server/src/api/picked-place/picked-place.service.ts
+++ apps/user-server/src/api/picked-place/picked-place.service.ts
@@ -1,35 +1,66 @@
 import { Injectable } from '@nestjs/common';
+import { GetAllPlaceOverviewDto } from '../place/dto/request/get-all-place-overview.dto';
+import { PickedPlaceOverviewEntity } from './entity/picked-place.overview.entity';
+import { BookmarkCoreService, PickedPlaceCoreService } from '@libs/core';
+import { GetAllPickedPlaceOverviewDto } from './dto/request/get-all-picked-place-overview.dto';
 
 @Injectable()
 export class PickedPlaceService {
-  constructor() {}
+  constructor(
+    private readonly pickedPlaceCoreService: PickedPlaceCoreService,
+    private readonly bookmarkCoreService: BookmarkCoreService,
+  ) {}
 
   /**
    * 선정된 장소 개요 (Picked Place) 모두 가져오기
    *
    * @author 강정연
    */
-  // async getAllPickedPlaceOverview(
-  //   page: number,
-  //   userIdx?: number,
-  // ): Promise<GetAllPickedPlaceOverviewResponseDto> {
-  //   const pageSize = 10;
-  //   const take = pageSize + 1;
-  //   const skip = (page - 1) * pageSize;
+  async getAllPickedPlaceOverview(
+    dto: GetAllPickedPlaceOverviewDto,
+    userIdx?: number,
+  ): Promise<{
+    pickedPlaceOverviewList: PickedPlaceOverviewEntity[];
+    hasNext: boolean;
+  }> {
+    const pageSize = 10;
+    const take = pageSize + 1;
+    const skip = (dto.page - 1) * pageSize;
 
-  //   let placeList =
-  //     await this.pickedPlaceRepository.selectAllPickedPlaceOverview(
-  //       skip,
-  //       take,
-  //       userIdx,
-  //     );
+    const pickedPlaceModelList =
+      await this.pickedPlaceCoreService.getPickedPlaceAll({
+        take,
+        skip,
+        ...dto,
+      });
 
-  //   const hasNext = !!placeList[pageSize];
-  //   placeList = placeList.slice(0, pageSize);
+    const hasNext = pickedPlaceModelList.length > pageSize;
+    const paginatedList = pickedPlaceModelList.slice(0, pageSize);
 
-  //   return {
-  //     hasNext,
-  //     data: placeList.map(PickedPlaceOverviewEntity.createEntityFromPrisma),
-  //   };
-  // }
+    if (!userIdx) {
+      return {
+        pickedPlaceOverviewList: paginatedList.map((pickedPlace) =>
+          PickedPlaceOverviewEntity.fromModel(pickedPlace, false),
+        ),
+        hasNext,
+      };
+    }
+
+    const bookmarkedPlaceIdxList = await this.bookmarkCoreService
+      .getBookmarkStateByUserIdx({
+        userIdx,
+        placeIdxList: paginatedList.map(({ place }) => place.idx),
+      })
+      .then((bookmarks) => bookmarks.map(({ placeIdx }) => placeIdx));
+
+    return {
+      pickedPlaceOverviewList: paginatedList.map((pickedPlace) =>
+        PickedPlaceOverviewEntity.fromModel(
+          pickedPlace,
+          bookmarkedPlaceIdxList.includes(pickedPlace.place.idx),
+        ),
+      ),
+      hasNext,
+    };
+  }
 }
