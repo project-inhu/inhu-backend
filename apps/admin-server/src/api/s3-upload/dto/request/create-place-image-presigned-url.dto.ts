d55fc65321029f721d0f752a31c2631ec63b3a67
hj-lab <heeju03180@naver.com>
2025-07-29 22:02:43 +0900

    feat: Add CreatePlaceImagePresignedUrlsDto

---
+++ apps/admin-server/src/api/s3-upload/dto/request/create-place-image-presigned-url.dto.ts
@@ -0,0 +1,23 @@
+import {
+  IMAGE_EXTENSION,
+  ImageExtension,
+} from '@libs/common/modules/s3/constants/image-extension.constants';
+import { ArrayMaxSize, IsIn, IsNotEmpty, IsString } from 'class-validator';
+
+/**
+ * Presigned URL 여러개 생성을 위한 입력 값
+ *
+ * @author 조희주
+ */
+export class CreatePlaceImagePresignedUrlsDto {
+  /**
+   * 업로드할 파일의 확장자명
+   *
+   * @example "jpg"
+   */
+  @IsNotEmpty()
+  @IsString()
+  @IsIn(Object.values(IMAGE_EXTENSION))
+  @ArrayMaxSize(5)
+  extensions: ImageExtension[];
+}
