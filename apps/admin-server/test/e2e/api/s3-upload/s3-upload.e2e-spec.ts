b8e621533fe5027387d6a4744b21bbee7b418263
hj-lab <heeju03180@naver.com>
2025-07-29 17:23:06 +0900

    test: Add menuImagePresignedUrl test cases in admin-server s3-upload

--- apps/admin-server/test/e2e/api/s3-upload/s3-upload.e2e-spec.ts
+++ apps/admin-server/test/e2e/api/s3-upload/s3-upload.e2e-spec.ts
@@ -21,7 +21,7 @@ describe('s3-upload E2E test', () => {
       const mockS3Response = {
         url: 'https://your-bucket.s3.amazonaws.com/',
         fields: {
-          Key: `banner/some-uuid-for`,
+          Key: `banner/some-uuid-for.jpg`,
           Policy: 'base64-encoded-policy-string',
           'X-Amz-Signature': 'signature-string',
         },
@@ -75,6 +75,71 @@ describe('s3-upload E2E test', () => {
         .post('/s3-upload/banner-image/presigned-url')
         .set('Authorization', `Bearer ${loginUser.token}`)
         .send({ extension: 'gif' })
+        .expect(400);
+    });
+  });
+
+  describe('POST /s3-upload/menu-image/presigned-url', () => {
+    it('201 - successfully retrieves presigned post data for menu image upload', async () => {
+      const loginUser = testHelper.loginAdmin.admin1;
+
+      const mockS3Response = {
+        url: 'https://your-bucket.s3.amazonaws.com/',
+        fields: {
+          Key: `menu/some-uuid-for.jpg`,
+          Policy: 'base64-encoded-policy-string',
+          'X-Amz-Signature': 'signature-string',
+        },
+      };
+
+      const s3ServiceMock = jest
+        .spyOn(testHelper.get(S3Service), 'getPresignedUrl')
+        .mockResolvedValue(mockS3Response);
+
+      const response = await testHelper
+        .test()
+        .post('/s3-upload/menu-image/presigned-url')
+        .send({ extension: IMAGE_EXTENSION.JPG })
+        .set('Authorization', `Bearer ${loginUser.token}`)
+        .expect(201);
+
+      const responseBody = response.body;
+
+      expect(responseBody).toBeDefined();
+      expect(responseBody).toHaveProperty('url');
+      expect(responseBody).toHaveProperty('fields');
+
+      expect(responseBody.url).toEqual(mockS3Response.url);
+      expect(responseBody.fields.Key).toEqual(mockS3Response.fields.Key);
+
+      s3ServiceMock.mockRestore();
+    });
+
+    it('401 - no accessToken', async () => {
+      await testHelper
+        .test()
+        .post('/s3-upload/menu-image/presigned-url')
+        .expect(401);
+    });
+
+    it('400 - extension is not provided', async () => {
+      const loginUser = testHelper.loginAdmin.admin1;
+
+      await testHelper
+        .test()
+        .post('/s3-upload/menu-image/presigned-url')
+        .set('Authorization', `Bearer ${loginUser.token}`)
+        .expect(400);
+    });
+
+    it('400 - extension is not in IMAGE_EXTENSION type', async () => {
+      const loginUser = testHelper.loginAdmin.admin1;
+
+      await testHelper
+        .test()
+        .post('/s3-upload/menu-image/presigned-url')
+        .set('Authorization', `Bearer ${loginUser.token}`)
+        .send({ extension: 'webp' })
         .expect(400);
     });
   });
