3aa3bbb3089f545c47b074cde34f0218747a4757
hj-lab <heeju03180@naver.com>
2025-07-29 16:19:44 +0900

    feat: Add createBannerImagePresignedUrl in service

--- apps/admin-server/src/api/s3-upload/s3-upload.service.ts
+++ apps/admin-server/src/api/s3-upload/s3-upload.service.ts
@@ -1,4 +1,17 @@
+import { PresignedUrlModel, S3_FOLDER, S3Service } from '@libs/common';
 import { Injectable } from '@nestjs/common';
+import { CreateBannerImagePresignedUrlDto } from './dto/request/create-banner-image-presigned-url.dto';
 
 @Injectable()
-export class S3UploadService {}
+export class S3UploadService {
+  constructor(private readonly s3Service: S3Service) {}
+
+  public async createBannerImagePresignedUrl(
+    createBannerImagePresignedUrlDto: CreateBannerImagePresignedUrlDto,
+  ): Promise<PresignedUrlModel> {
+    return await this.s3Service.getPresignedUrl({
+      folder: S3_FOLDER.BANNER,
+      extension: createBannerImagePresignedUrlDto.extension,
+    });
+  }
+}
