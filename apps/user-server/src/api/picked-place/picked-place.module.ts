2bea55dc3f9e33e7004f3dfff5543031e5c5cde6
JEONG-YEON-KANG <kjykjy3314@naver.com>
2025-08-03 15:08:05 +0900

    fix: Apply changes according to path convention

--- apps/user-server/src/api/picked-place/picked-place.module.ts
+++ apps/user-server/src/api/picked-place/picked-place.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PickedPlaceController } from './picked-place.controller';
 import { PickedPlaceService } from './picked-place.service';
-import { BookmarkCoreModule, PickedPlaceCoreModule } from '@libs/core';
+import { BookmarkCoreModule } from '@libs/core/bookmark/bookmark-core.module';
+import { PickedPlaceCoreModule } from '@libs/core/picked-place/picked-place-core.module';
 
 @Module({
   imports: [PickedPlaceCoreModule, BookmarkCoreModule],
