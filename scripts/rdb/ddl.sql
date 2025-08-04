7cb47f240b973fa41e515fcb33630628e70065fa
SuinLee21 <suin7473@naver.com>
2025-08-04 22:57:12 +0900

    fix: Fix invalid table name

--- scripts/rdb/ddl.sql
+++ scripts/rdb/ddl.sql
@@ -371,7 +371,7 @@ ALTER TABLE place_tb
     FOREIGN KEY (road_address_idx)
     REFERENCES road_address_tb (idx);
 
-ALTER TABLE admin_tb
+ALTER TABLE admin_account_tb
   ADD CONSTRAINT FK_user_tb_TO_admin_tb
     FOREIGN KEY (idx)
     REFERENCES user_tb (idx);
