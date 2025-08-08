834aabb490843bb96f69411fee2e69431a5ac178
JEONG-YEON-KANG <kjykjy3314@naver.com>
2025-08-08 22:54:51 +0900

    refactor: Robust execution path and modify the route in command

--- scripts/dev-infra/seed/execute.sh
+++ scripts/dev-infra/seed/execute.sh
@@ -1,8 +1,10 @@
+cd "$(dirname "$0")"
+
 npx esbuild seed.ts \
   --bundle \
   --platform=node \
   --format=cjs \
-  --outfile=../../dist/scripts/dev-infra/seed/seed.js \
+  --outfile=../../../dist/scripts/dev-infra/seed/seed.js \
   --packages=external \
   --external:@prisma/* \
   --external:.prisma/* \
