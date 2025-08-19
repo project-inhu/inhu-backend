d62d8c732bbb5d7378b6831105d283408d1f835d
JEONG-YEON-KANG <kjykjy3314@naver.com>
2025-08-19 18:56:39 +0900

    feat: Add codes to modules

--- apps/batch-server/src/batch-server.module.ts
+++ apps/batch-server/src/batch-server.module.ts
@@ -1,4 +1,27 @@
 import { Module } from '@nestjs/common';
+import { PlaceCronModule } from './place-cron/place-cron.module';
+import { ConfigModule } from '@nestjs/config';
+import { ClsModule } from 'nestjs-cls';
+import { ClsPluginTransactional } from '@nestjs-cls/transactional';
+import { PrismaModule } from '@libs/common/modules/prisma/prisma.module';
+import { TransactionalAdapterPrisma } from '@nestjs-cls/transactional-adapter-prisma';
+import { PrismaService } from '@libs/common/modules/prisma/prisma.service';
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    PlaceCronModule,
+    ClsModule.forRoot({
+      plugins: [
+        new ClsPluginTransactional({
+          imports: [PrismaModule],
+          adapter: new TransactionalAdapterPrisma({
+            prismaInjectionToken: PrismaService,
+            sqlFlavor: 'postgresql',
+          }),
+        }),
+      ],
+    }),
+  ],
+})
 export class BatchServerModule {}
--- apps/batch-server/src/place-cron/place-cron.module.ts
+++ apps/batch-server/src/place-cron/place-cron.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PlaceCronSchedule } from './place-cron.schedule';
 import { PlaceCronService } from './place-cron.service';
+import { PlaceCoreModule } from '@libs/core/place/place-core.module';
+import { DateUtilModule } from '@libs/common/modules/date-util/date-util.module';
 
 @Module({
-  imports: [],
+  imports: [PlaceCoreModule, DateUtilModule],
   providers: [PlaceCronSchedule, PlaceCronService],
 })
 export class PlaceCronModule {}
