a949b72bfcd646f1c4fbb64530ba04042f417fae
JEONG-YEON-KANG <kjykjy3314@naver.com>
2025-08-21 01:47:46 +0900

    fix: Modify the cron service according to the timezone

--- apps/batch-server/src/place-cron/place-cron.service.ts
+++ apps/batch-server/src/place-cron/place-cron.service.ts
@@ -17,25 +17,24 @@ export class PlaceCronService {
    */
   public async AddNextBiWeeklyClosedDay() {
     try {
-      const today = new Date();
-      today.setHours(0, 0, 0, 0);
-
-      const nextClosedDate = new Date();
-      nextClosedDate.setHours(0, 0, 0, 0);
-      nextClosedDate.setDate(today.getDate() + 14);
-
-      const todayKoreanDateStr =
-        this.dateUtilService.transformKoreanDate(today);
-      const startOfDay = new Date(`${todayKoreanDateStr}T00:00:00+09:00`);
-      const endOfDay = new Date(`${todayKoreanDateStr}T23:59:59.999+09:00`);
-
-      const nextKoreanDateStr =
-        this.dateUtilService.transformKoreanDate(nextClosedDate);
-      const nextStartOfDay = new Date(`${nextKoreanDateStr}T00:00:00+09:00`);
-      const nextEndOfDay = new Date(`${nextKoreanDateStr}T23:59:59.999+09:00`);
-
       const BIWEEKLY = 0;
 
+      // "한국 기준 오늘 날짜"를 가져옴
+      const now = this.dateUtilService.getNow();
+      const todayKSTStr = this.dateUtilService.transformKoreanDate(now);
+
+      // 오늘 날짜의 시작(00:00:00)과 끝(23:59:59)을 나타내는 Date 객체를 생성함
+      const startOfDay = new Date(todayKSTStr);
+      const endOfDay = new Date(startOfDay);
+      endOfDay.setHours(23, 59, 59, 999);
+
+      // 14일 뒤 날짜의 시작과 끝을 계산함
+      const nextStartOfDay = new Date(startOfDay);
+      nextStartOfDay.setDate(nextStartOfDay.getDate() + 14);
+      const nextEndOfDay = new Date(nextStartOfDay);
+      nextEndOfDay.setHours(23, 59, 59, 999);
+
+      // "오늘이 격주 휴무일"이지만 "14일 뒤에는 휴무일이 아직 없는" 장소 목록을 DB에서 조회
       const placeToUpdateList =
         await this.placeCoreService.getPlaceIdxAllByWeeklyClosedDay(
           startOfDay,
@@ -51,6 +50,10 @@ export class PlaceCronService {
 
       for (const place of placeToUpdateList) {
         try {
+          // 14일 뒤 휴무 날짜를 계산함
+          const nextClosedDate = new Date(startOfDay);
+          nextClosedDate.setDate(nextClosedDate.getDate() + 14);
+
           await this.placeCoreService.createWeeklyClosedDayByPlaceIdx(
             place.idx,
             nextClosedDate,
