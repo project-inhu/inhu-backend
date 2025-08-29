3535f96f8061923df8bf39fca54208521ae2d35d
JEONG-YEON-KANG <kjykjy3314@naver.com>
2025-08-29 22:06:11 +0900

    feat: Add kakao-address service using kakao API

--- libs/modules/src/kakao-address/kakao-address.module.ts
+++ libs/modules/src/kakao-address/kakao-address.module.ts
@@ -1,9 +1,11 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { KakaoAddressService } from './kakao-address.service';
+import { ConfigModule } from '@nestjs/config';
+import kakaoAddressConfig from './config/kakao-address.config';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, ConfigModule.forFeature(kakaoAddressConfig)],
   providers: [KakaoAddressService],
   exports: [KakaoAddressService],
 })
--- libs/modules/src/kakao-address/kakao-address.service.ts
+++ libs/modules/src/kakao-address/kakao-address.service.ts
@@ -1,8 +1,57 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { SearchAddressResponseDto } from './dto/response/serach-address.dto';
+import { KakaoAddressAPIException } from './exception/kakao-address-api.exception';
 
 @Injectable()
 export class KakaoAddressService {
-  constructor(private readonly httpService: HttpService) {}
+  private readonly KAKAO_APPLICATION_REST_API_KEY: string;
+
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly configService: ConfigService,
+  ) {
+    this.KAKAO_APPLICATION_REST_API_KEY =
+      this.configService.get('kakaoAddress').key;
+  }
+
+  /**
+   * 주소를 좌표로 변환하는 메서드
+   *
+   * @author 강정연
+   *
+   * @param address 검색할 주소
+   */
+  public async searchAddress(
+    address: string,
+  ): Promise<SearchAddressResponseDto> {
+    try {
+      const result =
+        await this.httpService.axiosRef.get<SearchAddressResponseDto>(
+          'https://dapi.kakao.com/v2/local/search/address.json',
+          {
+            headers: {
+              Authorization: `KakaoAK ${this.KAKAO_APPLICATION_REST_API_KEY}`,
+            },
+            params: {
+              query: address,
+              size: 10,
+            },
+          },
+        );
+
+      if (result.data.documents.length === 0) {
+        throw new KakaoAddressAPIException('Fail to GET kakao address', {
+          requestContext: {
+            address,
+          },
+        });
+      }
+
+      return result.data;
+    } catch (err: any) {
+      throw new KakaoAddressAPIException('Fail to GET kakao address', err);
+    }
+  }
 }
