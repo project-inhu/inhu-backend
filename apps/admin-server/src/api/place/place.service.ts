eef6745d3bd756a3132a46972d515271fe338418
JEONG-YEON-KANG <kjykjy3314@naver.com>
2025-08-30 00:40:42 +0900

    feat: Add getAddreesInfo function and modify create/update place logic

--- apps/admin-server/src/api/place/place.service.ts
+++ apps/admin-server/src/api/place/place.service.ts
@@ -10,11 +10,17 @@ import { NotClosedPermanentlyPlaceException } from '@admin/api/place/exception/n
 import { PlaceNotFoundException } from '@admin/api/place/exception/place-not-found.exception';
 import { GetPlaceOverviewInput } from '@libs/core/place/inputs/get-place-overview.input';
 import { PlaceCoreService } from '@libs/core/place/place-core.service';
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { AddressSearchDocumentEntity } from 'libs/modules/src/kakao-address/entity/address-search-document.entity';
+import { KakaoAddressEntity } from 'libs/modules/src/kakao-address/entity/kakao-address.entity';
+import { KakaoAddressService } from 'libs/modules/src/kakao-address/kakao-address.service';
 
 @Injectable()
 export class PlaceService {
-  constructor(private readonly placeCoreService: PlaceCoreService) {}
+  constructor(
+    private readonly placeCoreService: PlaceCoreService,
+    private readonly kakaoAddressService: KakaoAddressService,
+  ) {}
 
   public async getPlaceAll(
     dto: GetPlaceOverviewDto,
@@ -47,6 +53,8 @@ export class PlaceService {
   }
 
   public async createPlace(dto: CreatePlaceDto): Promise<PlaceEntity> {
+    const addressDocument = await this.getAddressInfo(dto.roadAddress.name);
+
     return await this.placeCoreService
       .createPlace({
         name: dto.name,
@@ -57,8 +65,8 @@ export class PlaceService {
         roadAddress: {
           name: dto.roadAddress.name,
           detail: dto.roadAddress.detail,
-          addressX: dto.roadAddress.addressX,
-          addressY: dto.roadAddress.addressY,
+          addressX: parseFloat(addressDocument.x),
+          addressY: parseFloat(addressDocument.y),
         },
         activatedAt: null,
         permanentlyClosedAt: null,
@@ -96,6 +104,8 @@ export class PlaceService {
       throw new PlaceNotFoundException('Cannot find place with idx: ' + idx);
     }
 
+    const addressDocument = await this.getAddressInfo(dto.roadAddress.name);
+
     return await this.placeCoreService.updatePlaceByIdx(idx, {
       name: dto.name,
       tel: dto.tel,
@@ -105,8 +115,8 @@ export class PlaceService {
       roadAddress: {
         name: dto.roadAddress.name,
         detail: dto.roadAddress.detail,
-        addressX: dto.roadAddress.addressX,
-        addressY: dto.roadAddress.addressY,
+        addressX: parseFloat(addressDocument.x),
+        addressY: parseFloat(addressDocument.y),
       },
       closedDayList: dto.closedDayList.map(({ day, week }) => ({
         day,
@@ -211,5 +221,14 @@ export class PlaceService {
     return await this.placeCoreService.updatePlaceByIdx(idx, {
       permanentlyClosedAt: null,
     });
+  }
+
+  private async getAddressInfo(
+    address: string,
+  ): Promise<AddressSearchDocumentEntity> {
+    if (!address) throw new BadRequestException('Address is required');
+
+    const response = await this.kakaoAddressService.searchAddress(address);
+    return response.documents[0];
   }
 }
