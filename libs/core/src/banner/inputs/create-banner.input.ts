38be43fee73d27462c0c990d157e0d94e60226d0
JEONG-YEON-KANG <kjykjy3314@naver.com>
2025-09-16 13:06:40 +0900

    feat: Add core inputs

---
+++ libs/core/src/banner/inputs/create-banner.input.ts
@@ -0,0 +1,13 @@
+/**
+ * CreateBannerInput 클래스
+ *
+ * @publicApi
+ */
+export class CreateBannerInput {
+  name: string;
+  imagePath: string;
+  link: string | null;
+  startAt: string;
+  endAt: string | null;
+  activatedAt: Date | null;
+}
---
+++ libs/core/src/banner/inputs/update-banner.input.ts
@@ -0,0 +1,9 @@
+import { PartialType } from '@nestjs/swagger';
+import { CreateBannerInput } from './create-banner.input';
+
+/**
+ * UpdateBannerInput 클래스
+ *
+ * @publicApi
+ */
+export class UpdateBannerInput extends PartialType(CreateBannerInput) {}
