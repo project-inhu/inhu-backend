22c3e70e53c3954afd462454e5cbbc94327fff63
SuinLee21 <suin7473@naver.com>
2025-09-17 16:26:03 +0900

    feat: Add coupon-owner model

--- libs/core/src/coupon-owner/model/coupon-owner.model.ts
+++ libs/core/src/coupon-owner/model/coupon-owner.model.ts
@@ -1,4 +1,6 @@
-import { SelectCouponOwner } from '../../coupon-owner/model/prisma-type/select-coupon-owner';
+import { CouponModel } from '@libs/core/coupon/model/coupon.model';
+import { UserModel } from '@libs/core/user/model/user.model';
+import { SelectCouponOwner } from './prisma-type/select-coupon-owner';
 
 /**
  * 쿠폰 소유자 모델
@@ -6,10 +8,25 @@ import { SelectCouponOwner } from '../../coupon-owner/model/prisma-type/select-c
  * @publicApi
  */
 export class CouponOwnerModel {
+  /**
+   * 쿠폰 소유자 고유 식별자
+   */
   public id: string;
-  public couponId: string;
-  public userIdx: number;
+
+  /**
+   * 쿠폰 소유자 생성 일시
+   */
   public createdAt: Date;
+
+  /**
+   * 쿠폰 정보
+   */
+  public coupon: CouponModel;
+
+  /**
+   * 사용자 정보
+   */
+  public user: UserModel;
 
   constructor(data: CouponOwnerModel) {
     Object.assign(this, data);
@@ -18,9 +35,9 @@ export class CouponOwnerModel {
   public static fromPrisma(couponOwner: SelectCouponOwner): CouponOwnerModel {
     return new CouponOwnerModel({
       id: couponOwner.id,
-      couponId: couponOwner.couponId,
-      userIdx: couponOwner.userIdx,
       createdAt: couponOwner.createdAt,
+      coupon: CouponModel.fromPrisma(couponOwner.coupon),
+      user: UserModel.fromPrisma(couponOwner.user),
     });
   }
 }
--- libs/core/src/coupon-owner/model/prisma-type/select-coupon-owner.ts
+++ libs/core/src/coupon-owner/model/prisma-type/select-coupon-owner.ts
@@ -1,12 +1,14 @@
+import { SELECT_COUPON } from '@libs/core/coupon/model/prisma-type/select-coupon';
+import { SELECT_USER } from '@libs/core/user/model/prisma-type/select-user';
 import { Prisma } from '@prisma/client';
 
 export const SELECT_COUPON_OWNER =
   Prisma.validator<Prisma.CouponOwnerDefaultArgs>()({
     select: {
       id: true,
-      couponId: true,
-      userIdx: true,
       createdAt: true,
+      coupon: SELECT_COUPON,
+      user: SELECT_USER,
     },
   });
 
