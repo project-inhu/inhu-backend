8b53de56494e909c06a3cb1838eb64d6b3f4bfa7
SuinLee21 <suin7473@naver.com>
2025-10-09 15:28:46 +0900

    feat: Add repository method in core-service

--- libs/core/src/magazine/magazine-core.service.ts
+++ libs/core/src/magazine/magazine-core.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { MagazineCoreRepository } from './magazine-core.repository';
 import { MagazineModel } from './model/magazine.model';
+import { CreateMagazineInput } from './inputs/create-magazine.input';
 
 /**
  * @publicApi
@@ -21,5 +22,28 @@ export class MagazineCoreService {
     return (await this.magazineCoreRepository.selectMagazineAll()).map(
       MagazineModel.fromPrisma,
     );
+  }
+
+  // TODO: text 추출 알고리즘 필요
+  public async createMagazine(
+    input: CreateMagazineInput,
+  ): Promise<MagazineModel> {
+    return MagazineModel.fromPrisma(
+      await this.magazineCoreRepository.insertMagazine(input),
+    );
+  }
+
+  public async updateMagazineActivatedAtByIdx(
+    idx: number,
+    activate: boolean,
+  ): Promise<void> {
+    await this.magazineCoreRepository.updateMagazineActivatedAtByIdx(
+      idx,
+      activate,
+    );
+  }
+
+  public async deleteMagazineByIdx(idx: number): Promise<void> {
+    await this.magazineCoreRepository.softDeleteMagazineByIdx(idx);
   }
 }
