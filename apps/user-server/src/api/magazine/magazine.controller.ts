b8584a1566d07e75072a6e65db6dbc736738350b
SuinLee21 <suin7473@naver.com>
2025-10-10 15:25:07 +0900

    feat: Implement magazine api in user-server controller

--- apps/user-server/src/api/magazine/magazine.controller.ts
+++ apps/user-server/src/api/magazine/magazine.controller.ts
@@ -1,7 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
 import { MagazineService } from './magazine.service';
+import { GetAllMagazineOverviewDto } from './dto/request/get-all-magazine-overview.dto';
+import { LoginAuth } from '@user/common/decorator/login-auth.decorator';
+import { User } from '@user/common/decorator/user.decorator';
+import { LoginUser } from '@user/common/types/LoginUser';
 
 @Controller('magazine')
 export class MagazineController {
   constructor(private readonly magazineService: MagazineService) {}
+
+  @Get('/:idx')
+  @LoginAuth()
+  public async getMagazineByIdx(
+    @Param('idx', ParseIntPipe) idx: number,
+    @User() loginUser?: LoginUser,
+  ) {
+    return await this.magazineService.getMagazineByIdx(idx, loginUser);
+  }
+
+  @Get('/overview/all')
+  public async getMagazineOverviewAll(@Query() dto: GetAllMagazineOverviewDto) {
+    return await this.magazineService.getMagazineOverviewAll(dto);
+  }
 }
