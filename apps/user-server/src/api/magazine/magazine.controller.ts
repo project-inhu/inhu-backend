740c50e90a131e0ac6ada1f8d36bd171442b6682
SuinLee21 <suin7473@naver.com>
2025-10-10 16:10:53 +0900

    chore: Add return type and remove LoginAuth decorator

--- apps/user-server/src/api/magazine/magazine.controller.ts
+++ apps/user-server/src/api/magazine/magazine.controller.ts
@@ -1,25 +1,27 @@
 import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
 import { MagazineService } from './magazine.service';
 import { GetAllMagazineOverviewDto } from './dto/request/get-all-magazine-overview.dto';
-import { LoginAuth } from '@user/common/decorator/login-auth.decorator';
 import { User } from '@user/common/decorator/user.decorator';
 import { LoginUser } from '@user/common/types/LoginUser';
+import { MagazineEntity } from './entity/magazine.entity';
+import { MagazineOverviewEntity } from './entity/magazine-overview.entity';
 
 @Controller('magazine')
 export class MagazineController {
   constructor(private readonly magazineService: MagazineService) {}
 
   @Get('/:idx')
-  @LoginAuth()
   public async getMagazineByIdx(
     @Param('idx', ParseIntPipe) idx: number,
     @User() loginUser?: LoginUser,
-  ) {
+  ): Promise<MagazineEntity | null> {
     return await this.magazineService.getMagazineByIdx(idx, loginUser);
   }
 
   @Get('/overview/all')
-  public async getMagazineOverviewAll(@Query() dto: GetAllMagazineOverviewDto) {
+  public async getMagazineOverviewAll(
+    @Query() dto: GetAllMagazineOverviewDto,
+  ): Promise<MagazineOverviewEntity[]> {
     return await this.magazineService.getMagazineOverviewAll(dto);
   }
 }
