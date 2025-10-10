1d503618a416b0f9e546d92c521e19fcf9157c7f
SuinLee21 <suin7473@naver.com>
2025-10-10 15:03:57 +0900

    feat: Implement get magazine by idx in user-server

--- apps/user-server/src/api/magazine/magazine.module.ts
+++ apps/user-server/src/api/magazine/magazine.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MagazineController } from './magazine.controller';
 import { MagazineService } from './magazine.service';
 import { MagazineCoreModule } from '@libs/core/magazine/magazine-core.module';
+import { BookmarkCoreModule } from '@libs/core/bookmark/bookmark-core.module';
 
 @Module({
-  imports: [MagazineCoreModule],
+  imports: [MagazineCoreModule, BookmarkCoreModule],
   controllers: [MagazineController],
   providers: [MagazineService],
   exports: [MagazineService],
--- apps/user-server/src/api/magazine/magazine.service.ts
+++ apps/user-server/src/api/magazine/magazine.service.ts
@@ -1,7 +1,38 @@
 import { MagazineCoreService } from '@libs/core/magazine/magazine-core.service';
 import { Injectable } from '@nestjs/common';
+import { MagazineEntity } from './entity/magazine.entity';
+import { BookmarkCoreService } from '@libs/core/bookmark/bookmark-core.service';
+import { LoginUser } from '@user/common/types/LoginUser';
 
 @Injectable()
 export class MagazineService {
-  constructor(private readonly magazineCoreService: MagazineCoreService) {}
+  constructor(
+    private readonly magazineCoreService: MagazineCoreService,
+    private readonly bookmarkCoreService: BookmarkCoreService,
+  ) {}
+
+  // ! 가게 정보가 사라졌을 때 메거진에서는 어떻게 해야 되나?
+  public async getMagazineByIdx(
+    idx: number,
+    loginUser?: LoginUser,
+  ): Promise<MagazineEntity | null> {
+    const magazine = await this.magazineCoreService.getMagazineByIdx(idx);
+
+    if (!loginUser) {
+      return magazine && MagazineEntity.fromModel(magazine, []);
+    }
+
+    if (!magazine) {
+      return null;
+    }
+
+    const bookmarkedPlaceList = await this.bookmarkCoreService
+      .getBookmarkStateByUserIdx({
+        userIdx: loginUser.idx,
+        placeIdxList: magazine.placeList.map((place) => place.idx),
+      })
+      .then((bookmarks) => bookmarks.map((bookmark) => bookmark.placeIdx));
+
+    return magazine && MagazineEntity.fromModel(magazine, bookmarkedPlaceList);
+  }
 }
