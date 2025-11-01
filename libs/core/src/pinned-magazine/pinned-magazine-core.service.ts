373dfd10a10f294e380d5f96e5fbc704b14eed22
SuinLee21 <suin7473@naver.com>
2025-11-01 16:02:57 +0900

    feat: Implement pinned magazine module

---
+++ libs/core/src/pinned-magazine/pinned-magazine-core.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { PinnedMagazineCoreRepository } from './pinned-magazine-core.repository';
+import { PinnedMagazineModel } from './model/pinned-magazine.model';
+import { PinnedMagazineOverviewModel } from './model/pinned-magazine-overview.model';
+
+@Injectable()
+export class PinnedMagazineCoreService {
+  constructor(
+    private readonly pinnedMagazineCoreRepository: PinnedMagazineCoreRepository,
+  ) {}
+
+  public async getPinnedMagazineByIdx(
+    idx: number,
+  ): Promise<PinnedMagazineModel | null> {
+    const pinnedMagazine =
+      await this.pinnedMagazineCoreRepository.selectPinnedMagazineByIdx(idx);
+
+    return pinnedMagazine
+      ? PinnedMagazineModel.fromPrisma(pinnedMagazine)
+      : null;
+  }
+
+  public async getPinnedMagazineAll(): Promise<PinnedMagazineOverviewModel[]> {
+    return (
+      await this.pinnedMagazineCoreRepository.selectPinnedMagazineAll()
+    ).map((pinnedMagazine) =>
+      PinnedMagazineOverviewModel.fromPrisma(pinnedMagazine),
+    );
+  }
+
+  public async createPinnedMagazine(
+    magazineIdx: number,
+  ): Promise<PinnedMagazineModel> {
+    return PinnedMagazineModel.fromPrisma(
+      await this.pinnedMagazineCoreRepository.insertPinnedMagazine(magazineIdx),
+    );
+  }
+
+  public async deletePinnedMagazineByIdx(magazineIdx: number): Promise<void> {
+    await this.pinnedMagazineCoreRepository.deletePinnedMagazineIdx(
+      magazineIdx,
+    );
+  }
+}
